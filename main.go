6f39274d274a311c4f3598f7ac8b679b06a48ca6
Marques Johansson <marques@displague.com>
2018-01-18 22:33:09 -0500

    minor s/$PROVIDER/linode/ template changes

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"github.com/LinodeContent/terraform-provider-linode"
 	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-linode/linode"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: linode.Provider,
-	})
+		ProviderFunc: linode.Provider})
 }
