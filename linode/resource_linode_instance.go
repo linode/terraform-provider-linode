f5a2e405cf9e1356f6fb9170ed1c2e38289fb249
Marques Johansson <marques@displague.com>
2018-08-02 15:56:45 -0400

    fix: treat instance tags as a terraform Set not List (unordered)

--- linode/resource_linode_instance.go
+++ linode/resource_linode_instance.go
@@ -69,7 +69,7 @@ func resourceLinodeInstance() *schema.Resource {
 				Optional:    true,
 			},
 			"tags": &schema.Schema{
-				Type:        schema.TypeList,
+				Type:        schema.TypeSet,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 				Description: "The tags to apply to the Linode instance.",
 				Optional:    true,
