ae3da71f8df61964df06af5c99aaf01ff3a6a362
Marques Johansson <marques@displague.com>
2018-10-28 00:06:54 -0400

    add the token resource

---
+++ linode/resource_linode_token.go
@@ -0,0 +1,192 @@
+package linode
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/linode/linodego"
+)
+
+func resourceLinodeToken() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLinodeTokenCreate,
+		Read:   resourceLinodeTokenRead,
+		Update: resourceLinodeTokenUpdate,
+		Delete: resourceLinodeTokenDelete,
+		Exists: resourceLinodeTokenExists,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"label": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "The label of the Linode Token.",
+				Optional:    true,
+			},
+			"scopes": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "The scopes this token was created with. These define what parts of the Account the token can be used to access. Many command-line tools, such as the Linode CLI, require tokens with access to *. Tokens with more restrictive scopes are generally more secure.",
+				Required:    true,
+				ForceNew:    true,
+			},
+			"expiry": &schema.Schema{
+				Type:             schema.TypeString,
+				Description:      "When this token will expire. Personal Access Tokens cannot be renewed, so after this time the token will be completely unusable and a new token will need to be generated. Tokens may be created with 'null' as their expiry and will never expire unless revoked.",
+				Optional:         true,
+				ValidateFunc:     validDateTime,
+				ForceNew:         true,
+				DiffSuppressFunc: equivalentDate,
+			},
+			"created": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "The date and time this token was created.",
+				Computed:    true,
+			},
+			"token": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "The token used to access the API.",
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func equivalentDate(k, old, new string, d *schema.ResourceData) bool {
+	if dtOld, err := time.Parse("2006-01-02T15:04:05", old); err != nil {
+		log.Printf("[WARN] could not parse date %s: %s", old, err)
+		return false
+	} else if dtNew, err := time.Parse("2006-01-02T15:04:05", new); err != nil {
+		log.Printf("[WARN] could not parse date %s: %s", new, err)
+		return false
+	} else {
+		return dtOld.Equal(dtNew)
+	}
+}
+
+func validDateTime(i interface{}, k string) (s []string, es []error) {
+	v, ok := i.(string)
+	if !ok {
+		es = append(es, fmt.Errorf("expected type of %s to be string", k))
+		return
+	}
+	if _, err := time.Parse("2006-01-02T15:04:05Z", v); err != nil {
+		es = append(es, fmt.Errorf("expected %s to be a datetime, got %s", k, v))
+	}
+
+	return
+}
+
+func resourceLinodeTokenExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	client := meta.(linodego.Client)
+	id, err := strconv.ParseInt(d.Id(), 10, 64)
+	if err != nil {
+		return false, fmt.Errorf("Error parsing Linode Token ID %s as int: %s", d.Id(), err)
+	}
+
+	_, err = client.GetToken(context.Background(), int(id))
+	if err != nil {
+		if lerr, ok := err.(*linodego.Error); ok && lerr.Code == 404 {
+			d.SetId("")
+			return false, nil
+		}
+
+		return false, fmt.Errorf("Error getting Linode Token ID %s: %s", d.Id(), err)
+	}
+	return true, nil
+}
+
+func resourceLinodeTokenRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(linodego.Client)
+	id, err := strconv.ParseInt(d.Id(), 10, 64)
+	if err != nil {
+		return fmt.Errorf("Error parsing Linode Token ID %s as int: %s", d.Id(), err)
+	}
+
+	token, err := client.GetToken(context.Background(), int(id))
+
+	if err != nil {
+		return fmt.Errorf("Error finding the specified Linode Token: %s", err)
+	}
+
+	d.Set("label", token.Label)
+	d.Set("scopes", token.Scopes)
+	d.Set("created", token.Created.Format(time.RFC3339))
+	d.Set("expiry", token.Expiry.Format(time.RFC3339))
+
+	return nil
+}
+
+func resourceLinodeTokenCreate(d *schema.ResourceData, meta interface{}) error {
+	client, ok := meta.(linodego.Client)
+	if !ok {
+		return fmt.Errorf("Invalid Client when creating Linode Token")
+	}
+
+	createOpts := linodego.TokenCreateOptions{
+		Label:  d.Get("label").(string),
+		Scopes: d.Get("scopes").(string),
+	}
+
+	if expiryRaw, ok := d.GetOk("expiry"); ok {
+		if expiry, ok := expiryRaw.(string); !ok {
+			return fmt.Errorf("expected expiry to be a string, got %s", expiryRaw)
+		} else if dt, err := time.Parse("2006-01-02T15:04:05Z", expiry); err != nil {
+			return fmt.Errorf("expected expiry to be a datetime, got %s", expiry)
+		} else {
+			createOpts.Expiry = &dt
+		}
+	}
+
+	token, err := client.CreateToken(context.Background(), createOpts)
+	if err != nil {
+		return fmt.Errorf("Error creating a Linode Token: %s", err)
+	}
+	d.SetId(fmt.Sprintf("%d", token.ID))
+	d.Set("token", token.Token)
+
+	return resourceLinodeTokenRead(d, meta)
+}
+
+func resourceLinodeTokenUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(linodego.Client)
+
+	id, err := strconv.ParseInt(d.Id(), 10, 64)
+	if err != nil {
+		return fmt.Errorf("Error parsing Linode Token id %s as int: %s", d.Id(), err)
+	}
+
+	token, err := client.GetToken(context.Background(), int(id))
+	if err != nil {
+		return fmt.Errorf("Error fetching data about the current linode: %s", err)
+	}
+
+	updateOpts := token.GetUpdateOptions()
+	if d.HasChange("label") {
+		updateOpts.Label = d.Get("label").(string)
+
+		if token, err = client.UpdateToken(context.Background(), token.ID, updateOpts); err != nil {
+			return err
+		}
+		d.Set("label", token.Label)
+	}
+
+	return resourceLinodeTokenRead(d, meta)
+}
+
+func resourceLinodeTokenDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(linodego.Client)
+	id, err := strconv.ParseInt(d.Id(), 10, 64)
+	if err != nil {
+		return fmt.Errorf("Error parsing Linode Token id %s as int", d.Id())
+	}
+	err = client.DeleteToken(context.Background(), int(id))
+	if err != nil {
+		return fmt.Errorf("Error deleting Linode Token %d: %s", id, err)
+	}
+
+	return nil
+}
---
+++ linode/resource_linode_token_test.go
@@ -0,0 +1,124 @@
+package linode
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/linode/linodego"
+)
+
+func TestAccLinodeToken_basic(t *testing.T) {
+	t.Parallel()
+
+	resName := "linode_token.foobar"
+	var tokenName = acctest.RandomWithPrefix("tf_test")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLinodeTokenDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckLinodeTokenConfigBasic(tokenName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLinodeTokenExists,
+					resource.TestCheckResourceAttr(resName, "label", tokenName),
+					resource.TestCheckResourceAttr(resName, "expiry", "2100-01-02T03:04:05Z"),
+					resource.TestCheckResourceAttrSet(resName, "scopes"),
+					resource.TestCheckResourceAttrSet(resName, "token"),
+				),
+			},
+			{
+				ResourceName:            resName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"token"},
+			},
+			{
+				Config: testAccCheckLinodeTokenConfigUpdates(tokenName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLinodeTokenExists,
+					resource.TestCheckResourceAttr(resName, "label", fmt.Sprintf("%s_renamed", tokenName)),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLinodeTokenExists(s *terraform.State) error {
+	client := testAccProvider.Meta().(linodego.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "linode_token" {
+			continue
+		}
+
+		id, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("Error parsing %v to int", rs.Primary.ID)
+		}
+
+		_, err = client.GetToken(context.Background(), id)
+		if err != nil {
+			return fmt.Errorf("Error retrieving state of Token %s: %s", rs.Primary.Attributes["label"], err)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLinodeTokenDestroy(s *terraform.State) error {
+	client, ok := testAccProvider.Meta().(linodego.Client)
+	if !ok {
+		return fmt.Errorf("Error getting Linode client")
+	}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "linode_token" {
+			continue
+		}
+
+		id, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("Error parsing %v to int", rs.Primary.ID)
+		}
+		if id == 0 {
+			return fmt.Errorf("Would have considered %v as %d", rs.Primary.ID, id)
+
+		}
+
+		_, err = client.GetToken(context.Background(), id)
+
+		if err == nil {
+			return fmt.Errorf("Linode Token with id %d still exists", id)
+		}
+
+		if apiErr, ok := err.(*linodego.Error); ok && apiErr.Code != 404 {
+			return fmt.Errorf("Error requesting Linode Token with id %d", id)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLinodeTokenConfigBasic(token string) string {
+	return fmt.Sprintf(`
+	resource "linode_token" "foobar" {
+		label = "%s"
+		scopes = "linodes:read_only"
+		expiry = "2100-01-02T03:04:05Z"
+	}`, token)
+}
+
+func testAccCheckLinodeTokenConfigUpdates(token string) string {
+	return fmt.Sprintf(`
+	resource "linode_token" "foobar" {
+		label = "%s_renamed"
+		scopes = "linodes:read_only"
+		expiry = "2100-01-02T03:04:05Z"
+	}`, token)
+}
