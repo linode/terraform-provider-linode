17a925008e1c3aeb79f14d85cf42edabc88ae497
phillc <15082+phillc@users.noreply.github.com>
2019-09-30 09:17:45 -0400

    Implement Object Storage Bucket

---
+++ linode/resource_linode_object_storage_bucket.go
@@ -0,0 +1,119 @@
+package linode
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/linode/linodego"
+)
+
+func resourceLinodeObjectStorageBucket() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLinodeObjectStorageBucketCreate,
+		Read:   resourceLinodeObjectStorageBucketRead,
+		Delete: resourceLinodeObjectStorageBucketDelete,
+		Exists: resourceLinodeObjectStorageBucketExists,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"cluster": {
+				Type:        schema.TypeString,
+				Description: "The cluster of the Linode Object Storage Bucket.",
+				Required:    true,
+				ForceNew:    true,
+			},
+			"label": {
+				Type:        schema.TypeString,
+				Description: "The label of the Linode Object Storage Bucket.",
+				Required:    true,
+				ForceNew:    true,
+			},
+		},
+	}
+}
+
+func resourceLinodeObjectStorageBucketExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	client := meta.(linodego.Client)
+	cluster := d.Get("cluster").(string)
+	label := d.Get("label").(string)
+
+	_, err := client.GetObjectStorageBucket(context.Background(), cluster, label)
+	if err != nil {
+		if lerr, ok := err.(*linodego.Error); ok && lerr.Code == 404 {
+			d.SetId("")
+			return false, nil
+		}
+
+		return false, fmt.Errorf("Error getting Linode ObjectStorageBucket %s/%s: %s", cluster, label, err)
+	}
+	return true, nil
+}
+
+func resourceLinodeObjectStorageBucketRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(linodego.Client)
+	cluster, label, err := decodeLinodeObjectStorageBucketID(d.Id())
+
+	if err != nil {
+		return fmt.Errorf("Error parsing Linode ObjectStorageBucket id %s", d.Id())
+	}
+
+	bucket, err := client.GetObjectStorageBucket(context.Background(), cluster, label)
+
+	if err != nil {
+		return fmt.Errorf("Error finding the specified Linode ObjectStorageBucket: %s", err)
+	}
+
+	d.SetId(fmt.Sprintf("%s:%s", bucket.Cluster, bucket.Label))
+	d.Set("cluster", bucket.Cluster)
+	d.Set("label", bucket.Label)
+
+	return nil
+}
+
+func resourceLinodeObjectStorageBucketCreate(d *schema.ResourceData, meta interface{}) error {
+	client, ok := meta.(linodego.Client)
+	if !ok {
+		return fmt.Errorf("Invalid Client when creating Linode ObjectStorageBucket")
+	}
+
+	createOpts := linodego.ObjectStorageBucketCreateOptions{
+		Cluster: d.Get("cluster").(string),
+		Label:   d.Get("label").(string),
+	}
+	bucket, err := client.CreateObjectStorageBucket(context.Background(), createOpts)
+	if err != nil {
+		return fmt.Errorf("Error creating a Linode ObjectStorageBucket: %s", err)
+	}
+	d.SetId(fmt.Sprintf("%s:%s", bucket.Cluster, bucket.Label))
+	d.Set("cluster", bucket.Cluster)
+	d.Set("label", bucket.Label)
+
+	return resourceLinodeObjectStorageBucketRead(d, meta)
+}
+
+func resourceLinodeObjectStorageBucketDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(linodego.Client)
+	cluster, label, err := decodeLinodeObjectStorageBucketID(d.Id())
+	if err != nil {
+		return fmt.Errorf("Error parsing Linode ObjectStorageBucket id %s", d.Id())
+	}
+	err = client.DeleteObjectStorageBucket(context.Background(), cluster, label)
+	if err != nil {
+		return fmt.Errorf("Error deleting Linode ObjectStorageBucket %s: %s", d.Id(), err)
+	}
+	return nil
+}
+
+func decodeLinodeObjectStorageBucketID(id string) (cluster, label string, err error) {
+	parts := strings.Split(id, ":")
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		err = fmt.Errorf("Linode Object Storage Bucket ID must be of the form <Cluster>:<Label>, was provided: %s", id)
+		return
+	}
+	cluster = parts[0]
+	label = parts[1]
+	return
+}
---
+++ linode/resource_linode_object_storage_bucket_test.go
@@ -0,0 +1,211 @@
+package linode
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/linode/linodego"
+)
+
+func init() {
+	resource.AddTestSweepers("linode_object_storage_bucket", &resource.Sweeper{
+		Name: "linode_object_storage_bucket",
+		F:    testSweepLinodeObjectStorageBucket,
+	})
+}
+
+func testSweepLinodeObjectStorageBucket(prefix string) error {
+	client, err := getClientForSweepers()
+	if err != nil {
+		return fmt.Errorf("Error getting client: %s", err)
+	}
+
+	listOpts := sweeperListOptions(prefix, "label")
+	objectStorageBuckets, err := client.ListObjectStorageBuckets(context.Background(), listOpts)
+	if err != nil {
+		return fmt.Errorf("Error getting object_storage_buckets: %s", err)
+	}
+	for _, objectStorageBucket := range objectStorageBuckets {
+		if !shouldSweepAcceptanceTestResource(prefix, objectStorageBucket.Label) {
+			continue
+		}
+		err := client.DeleteObjectStorageBucket(context.Background(), objectStorageBucket.Cluster, objectStorageBucket.Label)
+
+		if err != nil {
+			return fmt.Errorf("Error destroying %s during sweep: %s", objectStorageBucket.Label, err)
+		}
+	}
+
+	return nil
+}
+
+func TestAccLinodeObjectStorageBucket_basic(t *testing.T) {
+	t.Parallel()
+
+	resName := "linode_object_storage_bucket.foobar"
+	var objectStorageBucketName = acctest.RandomWithPrefix("tf-test")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLinodeObjectStorageBucketDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckLinodeObjectStorageBucketConfigBasic(objectStorageBucketName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLinodeObjectStorageBucketExists,
+					resource.TestCheckResourceAttr(resName, "label", objectStorageBucketName),
+				),
+			},
+			{
+				ResourceName:      resName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccLinodeObjectStorageBucket_dataSource(t *testing.T) {
+	t.Parallel()
+
+	resName := "linode_object_storage_bucket.foobar"
+	var objectStorageBucketName = acctest.RandomWithPrefix("tf-test")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLinodeObjectStorageBucketDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckLinodeObjectStorageBucketConfigDataSource(objectStorageBucketName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLinodeObjectStorageBucketExists,
+					resource.TestCheckResourceAttr(resName, "label", objectStorageBucketName),
+				),
+			},
+			{
+				ResourceName:      resName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccLinodeObjectStorageBucket_update(t *testing.T) {
+	t.Parallel()
+
+	var objectStorageBucketName = acctest.RandomWithPrefix("tf-test")
+	resName := "linode_object_storage_bucket.foobar"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLinodeObjectStorageBucketDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckLinodeObjectStorageBucketConfigBasic(objectStorageBucketName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLinodeObjectStorageBucketExists,
+					resource.TestCheckResourceAttr(resName, "label", objectStorageBucketName),
+				),
+			},
+			{
+				Config: testAccCheckLinodeObjectStorageBucketConfigUpdates(objectStorageBucketName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLinodeObjectStorageBucketExists,
+					resource.TestCheckResourceAttr(resName, "label", fmt.Sprintf("%s-renamed", objectStorageBucketName)),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLinodeObjectStorageBucketExists(s *terraform.State) error {
+	client := testAccProvider.Meta().(linodego.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "linode_object_storage_bucket" {
+			continue
+		}
+
+		cluster, label, err := decodeLinodeObjectStorageBucketID(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("Error parsing %s, %s", rs.Primary.ID, err)
+		}
+
+		_, err = client.GetObjectStorageBucket(context.Background(), cluster, label)
+		if err != nil {
+			return fmt.Errorf("Error retrieving state of ObjectStorageBucket %s: %s", rs.Primary.Attributes["label"], err)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLinodeObjectStorageBucketDestroy(s *terraform.State) error {
+	client, ok := testAccProvider.Meta().(linodego.Client)
+	if !ok {
+		return fmt.Errorf("Error getting Linode client")
+	}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "linode_object_storage_bucket" {
+			continue
+		}
+
+		id := rs.Primary.ID
+		cluster, label, err := decodeLinodeObjectStorageBucketID(id)
+		if err != nil {
+			return fmt.Errorf("Error parsing %s", id)
+		}
+		if label == "" {
+			return fmt.Errorf("Would have considered %s as %s", id, label)
+
+		}
+
+		_, err = client.GetObjectStorageBucket(context.Background(), cluster, label)
+
+		if err == nil {
+			return fmt.Errorf("Linode ObjectStorageBucket with id %s still exists", id)
+		}
+
+		if apiErr, ok := err.(*linodego.Error); ok && apiErr.Code != 404 {
+			return fmt.Errorf("Error requesting Linode ObjectStorageBucket with id %s", id)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLinodeObjectStorageBucketConfigBasic(object_storage_bucket string) string {
+	return fmt.Sprintf(`
+resource "linode_object_storage_bucket" "foobar" {
+	cluster = "us-east-1"
+	label = "%s"
+}`, object_storage_bucket)
+}
+
+func testAccCheckLinodeObjectStorageBucketConfigUpdates(object_storage_bucket string) string {
+	return fmt.Sprintf(`
+resource "linode_object_storage_bucket" "foobar" {
+	cluster = "us-east-1"
+	label = "%s-renamed"
+}`, object_storage_bucket)
+}
+
+func testAccCheckLinodeObjectStorageBucketConfigDataSource(object_storage_bucket string) string {
+	return fmt.Sprintf(`
+data "linode_object_storage_cluster" "baz" {
+    id = "us-east-1"
+}
+
+resource "linode_object_storage_bucket" "foobar" {
+	cluster = data.linode_object_storage_cluster.baz.id
+	label = "%s"
+}`, object_storage_bucket)
+}
