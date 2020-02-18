dfb1e1cdb628abc4e6a7dd2c98e79b8a985ecc05
Charles Kenney <charlesc.kenney@gmail.com>
2020-02-18 11:09:09 -0500

    harden linode_stackscript datasource test

--- linode/data_source_linode_stackscript_test.go
+++ linode/data_source_linode_stackscript_test.go
@@ -1,6 +1,7 @@
 package linode
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -18,28 +19,47 @@ func TestAccDataSourceLinodeStackscript_basic(t *testing.T) {
 			{
 				Config: testDataSourceLinodeStackScriptBasic(),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "deployments_active"),
+					resource.TestCheckResourceAttrSet(resourceName, "deployments_total"),
+					resource.TestCheckResourceAttrSet(resourceName, "username"),
 					resource.TestCheckResourceAttr(resourceName, "label", "my_stackscript"),
 					resource.TestCheckResourceAttr(resourceName, "description", "test"),
-					resource.TestCheckResourceAttr(resourceName, "script", "#!/bin/bash\necho hello world\n"),
+					resource.TestCheckResourceAttr(resourceName, "is_public", "false"),
+					resource.TestCheckResourceAttr(resourceName, "rev_note", "initial"),
+					resource.TestCheckResourceAttr(resourceName, "script", testDataSourceLinodeStackScriptBasicScript),
+					resource.TestCheckResourceAttr(resourceName, "images.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "images.0", "linode/ubuntu18.04"),
+					resource.TestCheckResourceAttr(resourceName, "images.1", "linode/ubuntu16.04lts"),
+					resource.TestCheckResourceAttr(resourceName, "user_defined_fields.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "user_defined_fields.0.name", "name"),
+					resource.TestCheckResourceAttr(resourceName, "user_defined_fields.0.label", "Your name"),
+					resource.TestCheckResourceAttr(resourceName, "user_defined_fields.0.default", "user"),
+					resource.TestCheckResourceAttr(resourceName, "user_defined_fields.0.example", "Linus Torvalds"),
 				),
 			},
 		},
 	})
 }
 
+var testDataSourceLinodeStackScriptBasicScript = `#!/bin/bash
+#<UDF name="name" label="Your name" example="Linus Torvalds" default="user">
+# NAME=
+echo "Hello, $NAME!"
+`
+
 func testDataSourceLinodeStackScriptBasic() string {
-	return `
+	return fmt.Sprintf(`
 resource "linode_stackscript" "stackscript" {
 	label = "my_stackscript"
 	script = <<EOF
-#!/bin/bash
-echo hello world
-EOF
-	images = ["linode/ubuntu18.04"]
+%sEOF
+	images = ["linode/ubuntu18.04", "linode/ubuntu16.04lts"]
 	description = "test"
+	rev_note = "initial"
 }
 
 data "linode_stackscript" "stackscript" {
 	id = linode_stackscript.stackscript.id
-}`
+}`, testDataSourceLinodeStackScriptBasicScript)
 }
