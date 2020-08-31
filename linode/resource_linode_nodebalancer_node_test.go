d42b3b2671c7fd7c56c6155af46232dd0e81436e
Charles Kenney <charlesc.kenney@gmail.com>
2020-08-31 10:37:58 -0400

    fix NodeBalancerNode acceptance tests

--- linode/resource_linode_nodebalancer_node_test.go
+++ linode/resource_linode_nodebalancer_node_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -28,10 +29,8 @@ func TestAccLinodeNodeBalancerNode_basic(t *testing.T) {
 			{
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckLinodeNodeBalancerNodeExists,
+					testAccCheckLinodeNodeBalancerNode,
 					resource.TestCheckResourceAttr(resName, "label", nodeName),
-					resource.TestCheckResourceAttr(resName, "address", "192.168.200.2:80"),
-
 					resource.TestCheckResourceAttrSet(resName, "status"),
 					resource.TestCheckResourceAttr(resName, "mode", "accept"),
 					resource.TestCheckResourceAttr(resName, "weight", "50"),
@@ -61,18 +60,16 @@ func TestAccLinodeNodeBalancerNode_update(t *testing.T) {
 			{
 				Config: testAccCheckLinodeNodeBalancerNodeBasic(nodeName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckLinodeNodeBalancerNodeExists,
+					testAccCheckLinodeNodeBalancerNode,
 					resource.TestCheckResourceAttr(resName, "label", nodeName),
-					resource.TestCheckResourceAttr(resName, "address", "192.168.200.2:80"),
 					resource.TestCheckResourceAttr(resName, "weight", "50"),
 				),
 			},
 			{
 				Config: testAccCheckLinodeNodeBalancerNodeUpdates(nodeName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckLinodeNodeBalancerNodeExists,
+					testAccCheckLinodeNodeBalancerNode,
 					resource.TestCheckResourceAttr(resName, "label", fmt.Sprintf("%s_r", nodeName)),
-					resource.TestCheckResourceAttr(resName, "address", "192.168.200.2:8080"),
 					resource.TestCheckResourceAttr(resName, "weight", "200"),
 				),
 			},
@@ -86,36 +83,71 @@ func TestAccLinodeNodeBalancerNode_update(t *testing.T) {
 	})
 }
 
-func testAccCheckLinodeNodeBalancerNodeExists(s *terraform.State) error {
+func testAccCheckLinodeNodeBalancerNode(s *terraform.State) (err error) {
 	client := testAccProvider.Meta().(linodego.Client)
 
+	var linodeID, nodebalancerID, nodeID, configID int
+	var expectedNodePort string
+
+	// find Linode instance ID
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "linode_instance" {
+			continue
+		}
+
+		linodeID, err = strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("Error parsing %v to int", rs.Primary.ID)
+		}
+	}
+
+	// find NodeBalancer Node ID
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "linode_nodebalancer_node" {
 			continue
 		}
 
-		id, err := strconv.Atoi(rs.Primary.ID)
+		nodeID, err = strconv.Atoi(rs.Primary.ID)
 		if err != nil {
 			return fmt.Errorf("Error parsing %v to int", rs.Primary.ID)
 		}
 
-		nodebalancerID, err := strconv.Atoi(rs.Primary.Attributes["nodebalancer_id"])
+		nodebalancerID, err = strconv.Atoi(rs.Primary.Attributes["nodebalancer_id"])
 		if err != nil {
 			return fmt.Errorf("Error parsing %v to int", rs.Primary.Attributes["nodebalancer_id"])
 		}
 
-		configID, err := strconv.Atoi(rs.Primary.Attributes["config_id"])
+		configID, err = strconv.Atoi(rs.Primary.Attributes["config_id"])
 		if err != nil {
 			return fmt.Errorf("Error parsing %v to int", rs.Primary.Attributes["config_id"])
 		}
 
-		_, err = client.GetNodeBalancerNode(context.Background(), nodebalancerID, configID, id)
-		if err != nil {
-			return fmt.Errorf("Error retrieving state of NodeBalancer Node %s: %s", rs.Primary.Attributes["label"], err)
-		}
+		expectedNodePort = strings.Split(rs.Primary.Attributes["address"], ":")[1]
 	}
 
-	return nil
+	instanceNetwork, err := client.GetInstanceIPAddresses(context.Background(), linodeID)
+	if err != nil {
+		return fmt.Errorf("failed to get IPs for instance %d: %s", linodeID, err)
+	}
+
+	node, err := client.GetNodeBalancerNode(context.Background(), nodebalancerID, configID, nodeID)
+	if err != nil {
+		return fmt.Errorf("Error retrieving state of NodeBalancer Node %d: %s", nodeID, err)
+	}
+
+	privateIP := instanceNetwork.IPv4.Private[0].Address
+
+	nodeAddrComps := strings.Split(node.Address, ":")
+	nodeHost, nodePort := nodeAddrComps[0], nodeAddrComps[1]
+
+	if nodeHost != privateIP {
+		return fmt.Errorf("expected node to have host '%s'; got '%s'", privateIP, node.Address)
+	}
+
+	if nodePort != expectedNodePort {
+		return fmt.Errorf("expected node to have port '%s'; got '%s'", expectedNodePort, nodePort)
+	}
+	return
 }
 
 func testAccCheckLinodeNodeBalancerNodeDestroy(s *terraform.State) error {
@@ -187,11 +219,11 @@ func testAccStateIDNodeBalancerNode(s *terraform.State) (string, error) {
 }
 
 func testAccCheckLinodeNodeBalancerNodeBasic(label string) string {
-	return testAccCheckLinodeNodeBalancerConfigBasic(label) + fmt.Sprintf(`
+	return testAccCheckLinodeInstanceConfigPrivateNetworking(label, publicKeyMaterial) + testAccCheckLinodeNodeBalancerConfigBasic(label) + fmt.Sprintf(`
 resource "linode_nodebalancer_node" "foonode" {
 	nodebalancer_id = "${linode_nodebalancer.foobar.id}"
 	config_id = "${linode_nodebalancer_config.foofig.id}"
-	address = "192.168.200.2:80"
+	address = "${linode_instance.foobar.private_ip_address}:80"
 	label = "%s"
 	weight = 50
 }
@@ -199,11 +231,11 @@ resource "linode_nodebalancer_node" "foonode" {
 }
 
 func testAccCheckLinodeNodeBalancerNodeUpdates(label string) string {
-	return testAccCheckLinodeNodeBalancerConfigBasic(label) + fmt.Sprintf(`
+	return testAccCheckLinodeInstanceConfigPrivateNetworking(label, publicKeyMaterial) + testAccCheckLinodeNodeBalancerConfigBasic(label) + fmt.Sprintf(`
 resource "linode_nodebalancer_node" "foonode" {
 	nodebalancer_id = "${linode_nodebalancer.foobar.id}"
 	config_id = "${linode_nodebalancer_config.foofig.id}"
-	address = "192.168.200.2:8080"
+	address = "${linode_instance.foobar.private_ip_address}:8080"
 	label = "%s_r"
 	weight = 200
 }
