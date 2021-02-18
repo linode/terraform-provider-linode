9a20c8488c4f46739a622bbc3719947afdb19baa
Charles Kenney <charlesc.kenney@gmail.com>
2021-02-18 11:08:06 -0500

    fix full disk swap upsize test

--- linode/helpers_test.go
+++ linode/helpers_test.go
@@ -2,10 +2,47 @@ package linode
 
 import (
 	"fmt"
+	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"golang.org/x/crypto/ssh"
 )
+
+func getSSHClient(t *testing.T, user, addr string) (client *ssh.Client) {
+	t.Helper()
+
+	signer, err := ssh.ParsePrivateKey([]byte(privateKeyMaterial))
+	if err != nil {
+		t.Fatalf("failed to parse private key: %s", err)
+	}
+	config := &ssh.ClientConfig{
+		User:            "root",
+		Auth:            []ssh.AuthMethod{ssh.PublicKeys(signer)},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         time.Minute,
+	}
+
+	attempts := 3
+
+	for attempts != 0 {
+		client, err = ssh.Dial("tcp", addr+":22", config)
+		if err == nil {
+			break
+		}
+
+		t.Logf("ssh dial failed: %s", err)
+		attempts--
+
+		time.Sleep(5 * time.Second)
+	}
+
+	if client == nil {
+		t.Fatal("failed to get ssh client")
+	}
+	return
+}
 
 func testAccCheckResourceAttrNotEqual(resName string, path, notValue string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
