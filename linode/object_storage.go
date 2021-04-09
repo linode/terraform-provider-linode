7fb7940c0bf03d7ffc19983771e76d7d843eb00f
lbgarber <lbgarber2@gmail.com>
2021-04-09 11:53:52 -0400

    Make Object Storage endpoint depend on cluster

--- linode/object_storage.go
+++ linode/object_storage.go
@@ -11,20 +11,21 @@ import (
 )
 
 const (
-	linodeObjectsEndpoint = "https://us-east-1.linodeobjects.com"
+	linodeObjectsEndpoint = "https://%s.linodeobjects.com"
 )
 
 // s3ConnFromResourceData builds an S3 client from the linode_object_storage_object
-// resource's access_key and secret_key fields.
+// resource's access_key, secret_key, and cluster fields.
 func s3ConnFromResourceData(d *schema.ResourceData) *s3.S3 {
 	accessKey := d.Get("access_key").(string)
 	secretKey := d.Get("secret_key").(string)
+	cluster := d.Get("cluster").(string)
 
 	sess := session.New(&aws.Config{
 		// This region is hardcoded strictly for preflight validation purposes.
 		Region:      aws.String("us-east-1"),
 		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
-		Endpoint:    aws.String(linodeObjectsEndpoint),
+		Endpoint:    aws.String(fmt.Sprintf(linodeObjectsEndpoint, cluster)),
 	})
 	return s3.New(sess)
 }
