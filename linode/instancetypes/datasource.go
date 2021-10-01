cead4dd32e83a6e28fde99c6fc01b54efc9dcc9b
LBGarber <LBGarber2@gmail.com>
2021-10-01 11:16:08 -0400

    Add linode_instance_types data source

---
+++ linode/instancetypes/datasource.go
@@ -0,0 +1,91 @@
+package instancetypes
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/linode/linodego"
+	"github.com/linode/terraform-provider-linode/linode/helper"
+
+	"context"
+	"strconv"
+)
+
+func DataSource() *schema.Resource {
+	return &schema.Resource{
+		Schema:      dataSourceSchema,
+		ReadContext: readDataSource,
+	}
+}
+
+func readDataSource(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*helper.ProviderMeta).Client
+
+	filter, err := helper.ConstructFilterString(d, typeValueToFilterType)
+	if err != nil {
+		return diag.Errorf("failed to construct filter: %s", err)
+	}
+
+	types, err := client.ListTypes(ctx, &linodego.ListOptions{
+		Filter: filter,
+	})
+
+	if err != nil {
+		return diag.Errorf("failed to list linode types: %s", err)
+	}
+
+	typesFlattened := make([]interface{}, len(types))
+	for i, t := range types {
+		typesFlattened[i] = flattenType(&t)
+	}
+
+	d.SetId(filter)
+	d.Set("types", typesFlattened)
+
+	return nil
+}
+
+func flattenType(t *linodego.LinodeType) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	result["id"] = t.ID
+	result["label"] = t.Label
+	result["disk"] = t.Disk
+	result["class"] = t.Class
+	result["network_out"] = t.NetworkOut
+	result["memory"] = t.Memory
+	result["transfer"] = t.Transfer
+	result["vcpus"] = t.VCPUs
+
+	result["price"] = []map[string]float32{
+		{
+			"hourly":  t.Price.Hourly,
+			"monthly": t.Price.Monthly,
+		},
+	}
+
+	result["addons"] = []map[string]interface{}{
+		{
+			"backups": []map[string]interface{}{
+				{
+					"price": []map[string]float32{
+						{
+							"hourly":  t.Addons.Backups.Price.Hourly,
+							"monthly": t.Addons.Backups.Price.Monthly,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return result
+}
+
+func typeValueToFilterType(filterName, value string) (interface{}, error) {
+	switch filterName {
+	case "disk", "gpus", "memory", "transfer", "vcpus":
+		return strconv.Atoi(value)
+	}
+
+	return value, nil
+}
---
+++ linode/instancetypes/datasource_test.go
@@ -0,0 +1,39 @@
+package instancetypes_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/linode/terraform-provider-linode/linode/acceptance"
+	"github.com/linode/terraform-provider-linode/linode/instancetypes/tmpl"
+)
+
+func TestAccDataSourceInstanceTypes_basic(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "data.linode_instance_types.foobar"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acceptance.PreCheck(t) },
+		Providers: acceptance.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: tmpl.DataBasic(t),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "types.0.id", "g6-standard-2"),
+					resource.TestCheckResourceAttr(resourceName, "types.0.label", "Linode 4GB"),
+					resource.TestCheckResourceAttr(resourceName, "types.0.class", "standard"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.disk"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.network_out"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.memory"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.transfer"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.vcpus"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.price.0.hourly"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.price.0.monthly"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.addons.0.backups.0.price.0.hourly"),
+					resource.TestCheckResourceAttrSet(resourceName, "types.0.addons.0.backups.0.price.0.monthly"),
+				),
+			},
+		},
+	})
+}
