b4d0ff9fef6f2922aa8a0e1f911b4d024e4239fd
LBGarber <lgarber@linode.com>
2021-12-08 09:43:50 -0500

    Remove goimports from tooldeps (implicit)

--- tools/tools.go
+++ tools/tools.go
@@ -12,5 +12,4 @@ import (
 	// see: https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md#tools-as-dependencies
 	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlint"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "golang.org/x/tools/cmd/goimports"
 )
