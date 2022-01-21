4f52ad8b0cc1e9c6ccbca9a219bc5a8d88003265
LBGarber <lgarber@linode.com>
2022-01-21 11:15:32 -0500

    make fmt

--- linode/instancetypes/datasource.go
+++ linode/instancetypes/datasource.go
@@ -2,7 +2,7 @@ package instancetypes
 
 import (
 	"context"
-	
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/linode/linodego"
