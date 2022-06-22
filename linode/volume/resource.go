f6062e363e0f9fcd5a1d1604719ac13c4dddfc0e
lbgarber <lgarber@linode.com>
2022-06-22 14:09:51 -0400

    Fix format

--- linode/volume/resource.go
+++ linode/volume/resource.go
@@ -278,7 +278,8 @@ func createVolume(ctx context.Context, d *schema.ResourceData, client linodego.C
 	return newVolume, nil
 }
 
-func createVolumeFromSource(ctx context.Context, d *schema.ResourceData, client linodego.Client) (*linodego.Volume, error) {
+func createVolumeFromSource(
+	ctx context.Context, d *schema.ResourceData, client linodego.Client) (*linodego.Volume, error) {
 	var clonedVolume *linodego.Volume
 
 	newRegion := d.Get("region").(string)
