f333cc7fdf885c91168c7fc19de47da731294998
LBGarber <lgarber@linode.com>
2022-07-26 14:05:50 -0400

    pls

--- linode/images/schema_datasource.go
+++ linode/images/schema_datasource.go
@@ -8,21 +8,21 @@ import (
 )
 
 var filterConfig = helper.FilterConfig{
-	"deprecated": {APIFilterable: true, TypeFunc: helper.FilterTypeBool},
-	"is_public":  {APIFilterable: true, TypeFunc: helper.FilterTypeBool},
-	"label":      {APIFilterable: true, TypeFunc: helper.FilterTypeString},
-	"size":       {APIFilterable: true, TypeFunc: helper.FilterTypeInt},
-	"type":       {APIFilterable: true, TypeFunc: helper.FilterTypeString},
-	"vendor":     {APIFilterable: true, TypeFunc: helper.FilterTypeString},
+	"deprecated": helper.FilterAttribute{APIFilterable: true, TypeFunc: helper.FilterTypeBool},
+	"is_public":  helper.FilterAttribute{APIFilterable: true, TypeFunc: helper.FilterTypeBool},
+	"label":      helper.FilterAttribute{APIFilterable: true, TypeFunc: helper.FilterTypeString},
+	"size":       helper.FilterAttribute{APIFilterable: true, TypeFunc: helper.FilterTypeInt},
+	"type":       helper.FilterAttribute{APIFilterable: true, TypeFunc: helper.FilterTypeString},
+	"vendor":     helper.FilterAttribute{APIFilterable: true, TypeFunc: helper.FilterTypeString},
 
-	"created_by": {TypeFunc: helper.FilterTypeString},
-	"id":         {TypeFunc: helper.FilterTypeString},
-	"status": {
+	"created_by": helper.FilterAttribute{TypeFunc: helper.FilterTypeString},
+	"id":         helper.FilterAttribute{TypeFunc: helper.FilterTypeString},
+	"status": helper.FilterAttribute{
 		TypeFunc: func(value string) (interface{}, error) {
 			return linodego.ImageStatus(value), nil
 		},
 	},
-	"description": {TypeFunc: helper.FilterTypeString},
+	"description": helper.FilterAttribute{TypeFunc: helper.FilterTypeString},
 }
 
 var dataSourceSchema = map[string]*schema.Schema{
