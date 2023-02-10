b01db7660aba1e6ba8e1f1e872062723d7e36f31
ezilber-akamai <ezilber@akamai.com>
2023-02-10 14:22:08 -0500

    Added support for label property in linode_region datasource

--- linode/region/datasource_test.go
+++ linode/region/datasource_test.go
@@ -1,6 +1,7 @@
 package region_test
 
 import (
+	"context"
 	"log"
 	"testing"
 
@@ -10,6 +11,7 @@ import (
 )
 
 var testRegion string
+var testLabel string
 
 func init() {
 	region, err := acceptance.GetRandomRegionWithCaps([]string{"linodes"})
@@ -18,12 +20,26 @@ func init() {
 	}
 
 	testRegion = region
+
+	client, err := acceptance.GetClientForSweepers()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := client.GetRegion(context.Background(), testRegion)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testLabel = r.Label
+
 }
 
 func TestAccDataSourceRegion_basic(t *testing.T) {
 	t.Parallel()
 
 	regionID := testRegion
+	label := testLabel
 	resourceName := "data.linode_region.foobar"
 
 	resource.Test(t, resource.TestCase{
@@ -31,10 +47,11 @@ func TestAccDataSourceRegion_basic(t *testing.T) {
 		Providers: acceptance.TestAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: tmpl.DataBasic(t, regionID),
+				Config: tmpl.DataBasic(t, regionID, label),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "country"),
 					resource.TestCheckResourceAttr(resourceName, "id", regionID),
+					resource.TestCheckResourceAttr(resourceName, "label", label),
 				),
 			},
 		},
