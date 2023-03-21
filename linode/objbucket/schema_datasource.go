873a78787d03c265bc3f6564493335a9d626c974
Ye Chen <yechen@akamai.com>
2023-03-21 14:28:40 -0400

    add untracked

---
+++ linode/objbucket/schema_datasource.go
@@ -0,0 +1,31 @@
+package objbucket
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+var bucketDataSourceSchema = map[string]*schema.Schema{
+	"cluster": {
+		Type:        schema.TypeString,
+		Description: "The ID of the Object Storage Cluster this bucket is in.",
+		Required:    true,
+	},
+	"created": {
+		Type:        schema.TypeString,
+		Description: "When this bucket was created.",
+		Computed:    true,
+	},
+	"hostname": {
+		Type:        schema.TypeString,
+		Description: "The hostname where this bucket can be accessed. This hostname can be accessed through a browser if the bucket is made public.",
+		Computed:    true,
+	},
+	"label": {
+		Type:        schema.TypeString,
+		Description: "The name of this bucket.",
+		Required:    true,
+	},
+	"id": {
+		Type:        schema.TypeString,
+		Description: "The id of this bucket.",
+		Computed:    true,
+	},
+}
