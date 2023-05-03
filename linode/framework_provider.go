d47b869cfc34121e9de2b6f5e354fbfa7106ecbd
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2023-05-03 14:48:45 -0400

    ref: Migrate "linode_stackscript" resource and data source to framework (#802)
    * Migrate "linode_stackscript" resource and data source to framework
    
    * Fix diag handling
    
    * Fix lint
    
    * Fix lint again

--- linode/framework_provider.go
+++ linode/framework_provider.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/linode/terraform-provider-linode/linode/helper"
+	"github.com/linode/terraform-provider-linode/linode/stackscript"
 	"github.com/linode/terraform-provider-linode/linode/token"
 )
 
@@ -98,14 +99,13 @@ func (p *FrameworkProvider) Schema(
 func (p *FrameworkProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		token.NewResource,
+		stackscript.NewResource,
 	}
 }
 
 func (p *FrameworkProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	// return nil
 	return []func() datasource.DataSource{
-		// func() datasource.DataSource {
-		//     return dataSourceExample{}
-		// },
+		stackscript.NewDataSource,
 	}
 }
--- linode/stackscripts/datasource.go
+++ linode/stackscripts/datasource.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/linode/linodego"
-	"github.com/linode/terraform-provider-linode/linode/stackscript"
 )
 
 func DataSource() *schema.Resource {
@@ -73,7 +72,27 @@ func flattenStackScript(data interface{}) map[string]interface{} {
 		result["updated"] = script.Updated.Format(time.RFC3339)
 	}
 
-	result["user_defined_fields"] = stackscript.GetStackScriptUserDefinedFields(&script)
+	result["user_defined_fields"] = getStackScriptUserDefinedFields(&script)
+
+	return result
+}
+
+func getStackScriptUserDefinedFields(ss *linodego.Stackscript) []map[string]string {
+	if ss.UserDefinedFields == nil {
+		return nil
+	}
+
+	result := make([]map[string]string, len(*ss.UserDefinedFields))
+	for i, udf := range *ss.UserDefinedFields {
+		result[i] = map[string]string{
+			"default": udf.Default,
+			"example": udf.Example,
+			"many_of": udf.ManyOf,
+			"one_of":  udf.OneOf,
+			"label":   udf.Label,
+			"name":    udf.Name,
+		}
+	}
 
 	return result
 }
