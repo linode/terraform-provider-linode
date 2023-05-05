9ee3c3bb34d731062665d3fd798791242b0aa172
ezilber-akamai <122484180+ezilber-akamai@users.noreply.github.com>
2023-05-05 13:53:39 -0400

    Migrated `account` datasource to Framework (#801)
    * Migrated account datasource to Framework
    
    * gofumpt
    
    * Fixed nil pointer error
    
    * Fixed naming issues
    
    * Removed redundant error message
    
    * Fixed helper function naming
    
    * Read config in Read method
    
    * Removed unneccesary data struct population
    
    ---------
    
    Co-authored-by: Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>

--- linode/framework_provider.go
+++ linode/framework_provider.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/linode/terraform-provider-linode/linode/account"
 	"github.com/linode/terraform-provider-linode/linode/helper"
 	"github.com/linode/terraform-provider-linode/linode/stackscript"
 	"github.com/linode/terraform-provider-linode/linode/token"
@@ -104,8 +105,8 @@ func (p *FrameworkProvider) Resources(ctx context.Context) []func() resource.Res
 }
 
 func (p *FrameworkProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	// return nil
 	return []func() datasource.DataSource{
+		account.NewDataSource,
 		stackscript.NewDataSource,
 	}
 }
