e33deca1bea3c748f69ff8a8615a82cd7aae2569
ezilber-akamai <ezilber@akamai.com>
2023-05-09 13:35:39 -0400

    fmt again

--- linode/framework_provider.go
+++ linode/framework_provider.go
@@ -11,7 +11,7 @@ import (
 	"github.com/linode/terraform-provider-linode/linode/helper"
 	"github.com/linode/terraform-provider-linode/linode/kernel"
 	"github.com/linode/terraform-provider-linode/linode/lkeversions"
-  "github.com/linode/terraform-provider-linode/linode/networkingip"
+	"github.com/linode/terraform-provider-linode/linode/networkingip"
 	"github.com/linode/terraform-provider-linode/linode/profile"
 	"github.com/linode/terraform-provider-linode/linode/stackscript"
 	"github.com/linode/terraform-provider-linode/linode/token"
