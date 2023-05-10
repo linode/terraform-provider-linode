16d050fd4500268a70bd60ab0162ac58be1d8be4
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2023-05-10 13:45:12 -0400

    Fix linting issue and rename file (#821)

--- linode/helper/framework_conversion.go
+++ linode/helper/framework_conversion.go
@@ -5,7 +5,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
-// GetValueIfNotNull - assign StringNull() safely without throwing error. e.g. new value: .rev_note: was null, but now cty.StringVal("")
+// GetValueIfNotNull - assign StringNull() safely without throwing error.
+// e.g. new value: .rev_note: was null, but now cty.StringVal("")
 func GetValueIfNotNull(val string) basetypes.StringValue {
 	res := types.StringValue(val)
 
