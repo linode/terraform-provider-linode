7d7778f062e4c19811833667c7386f4bbbf1f213
Zhiwei Liang <zliang@akamai.com>
2023-05-11 12:33:31 -0400

    Put resty retry condition func to another file

---
+++ linode/helper/retry.go
@@ -0,0 +1,31 @@
+package helper
+
+import (
+	"log"
+	"net/url"
+	"regexp"
+
+	"github.com/go-resty/resty/v2"
+)
+
+// Workaround for intermittent 5xx errors when retrieving a database from the API
+func Database502Retry() func(response *resty.Response, err error) bool {
+	databaseGetRegex, err := regexp.Compile("[A-Za-z0-9]+/databases/[a-z]+/instances/[0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return func(response *resty.Response, err error) bool {
+		if response.StatusCode() != 502 || response.Request == nil {
+			return false
+		}
+
+		requestURL, err := url.ParseRequestURI(response.Request.URL)
+		if err != nil {
+			log.Printf("[WARN] failed to parse request URL: %s", err)
+			return false
+		}
+
+		// Check whether the string matches
+		return databaseGetRegex.MatchString(requestURL.Path)
+	}
+}
