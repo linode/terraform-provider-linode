666c3fa2640a43d0cba1288af1f43fd221b91f11
ezilber-akamai <ezilber@akamai.com>
2023-05-19 09:13:58 -0400

    Removed unnecessary optional flags

--- linode/objcluster/framework_datasource_schema.go
+++ linode/objcluster/framework_datasource_schema.go
@@ -12,22 +12,18 @@ var frameworkDatasourceSchema = schema.Schema{
 		},
 		"domain": schema.StringAttribute{
 			Description: "The base URL for this cluster.",
-			Optional:    true,
 			Computed:    true,
 		},
 		"status": schema.StringAttribute{
 			Description: "This cluster's status.",
-			Optional:    true,
 			Computed:    true,
 		},
 		"region": schema.StringAttribute{
 			Description: "The region this cluster is located in.",
-			Optional:    true,
 			Computed:    true,
 		},
 		"static_site_domain": schema.StringAttribute{
 			Description: "The base URL for this cluster used when hosting static sites.",
-			Optional:    true,
 			Computed:    true,
 		},
 	},
