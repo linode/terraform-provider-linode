2afffefff000207361392d8285dfe6f62e1d94ff
Lena Garber <lgarber@akamai.com>
2023-05-22 13:45:57 -0400

    Add GetLatestVersion

--- linode/helper/frameworkfilter/latest.go
+++ linode/helper/frameworkfilter/latest.go
@@ -2,6 +2,7 @@ package frameworkfilter
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"reflect"
 	"time"
@@ -20,20 +21,52 @@ func (f Config) GetLatestCreated(elems []any, field string) (any, diag.Diagnosti
 	}
 
 	newestElem := elems[0]
+	newestCreated, d := getCreatedTime(newestElem, timeField.Name)
+	if d != nil {
+		return nil, d
+	}
 
 	for _, elem := range elems {
-		newestElemCreated, d := getCreatedTime(newestElem, timeField.Name)
-		if d != nil {
-			return nil, d
-		}
-
 		currentElemCreated, d := getCreatedTime(elem, timeField.Name)
 		if d != nil {
 			return nil, d
 		}
 
-		if currentElemCreated.After(newestElemCreated) {
+		if currentElemCreated.After(newestCreated) {
 			newestElem = elem
+			newestCreated = currentElemCreated
+		}
+	}
+
+	return newestElem, nil
+}
+
+// GetLatestVersion gets the latest version of the given struct
+func (f Config) GetLatestVersion(elems []any, field string) (any, diag.Diagnostic) {
+	if len(elems) < 1 {
+		return nil, nil
+	}
+
+	versionField, d := resolveStructFieldByJSON(elems[0], field)
+	if d != nil {
+		return nil, d
+	}
+
+	newestElem := elems[0]
+	newestVersion, d := getVersion(newestElem, versionField.Name)
+	if d != nil {
+		return nil, d
+	}
+
+	for _, elem := range elems {
+		currentVersion, d := getVersion(elem, versionField.Name)
+		if d != nil {
+			return nil, d
+		}
+
+		if currentVersion.GreaterThan(newestVersion) {
+			newestElem = elem
+			newestVersion = currentVersion
 		}
 	}
 
@@ -54,6 +87,35 @@ func getCreatedTime(elem any, attr string) (time.Time, diag.Diagnostic) {
 		return time.Time{}, diag.NewErrorDiagnostic(
 			"Field has incorrect type",
 			fmt.Sprintf("Field %s is not type time.Time", attr),
+		)
+	}
+
+	return result, nil
+}
+
+// getVersion parses a version value from the given elem and field.
+func getVersion(elem any, attr string) (*version.Version, diag.Diagnostic) {
+	val := reflect.ValueOf(elem).FieldByName(attr)
+
+	// Deref any pointers
+	for val.Kind() == reflect.Ptr {
+		val = reflect.Indirect(val)
+	}
+
+	// Check the type
+	if val.Kind() != reflect.String {
+		return nil, diag.NewErrorDiagnostic(
+			"Field has incorrect type",
+			fmt.Sprintf("Field %s is not type string", attr),
+		)
+	}
+
+	// Parse the version
+	result, err := version.NewVersion(val.String())
+	if err != nil {
+		return nil, diag.NewErrorDiagnostic(
+			"Failed to parse version",
+			err.Error(),
 		)
 	}
 
