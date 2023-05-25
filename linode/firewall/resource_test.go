b6ec7567709f698bba3b3a4d5270eaf085bee5ac
Zhiwei Liang <zliang@akamai.com>
2023-05-25 17:45:51 -0400

    Migrate tests to terraform-plugin-testing

--- linode/firewall/resource_test.go
+++ linode/firewall/resource_test.go
@@ -6,9 +6,9 @@ import (
 	"log"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/linode/linodego"
 	"github.com/linode/terraform-provider-linode/linode/acceptance"
 	"github.com/linode/terraform-provider-linode/linode/firewall/tmpl"
--- linode/profile/datasource_test.go
+++ linode/profile/datasource_test.go
@@ -3,7 +3,7 @@ package profile_test
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/linode/terraform-provider-linode/linode/acceptance"
 	"github.com/linode/terraform-provider-linode/linode/profile/tmpl"
 )
