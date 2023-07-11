0b66241d463b53cf22bc73e7e18bf7205f797356
Ye Chen <yechen@akamai.com>
2023-07-11 16:50:16 -0400

    clean up

--- linode/provider.go
+++ linode/provider.go
@@ -32,11 +32,6 @@ import (
 	"github.com/linode/terraform-provider-linode/linode/nbnode"
 	"github.com/linode/terraform-provider-linode/linode/obj"
 	"github.com/linode/terraform-provider-linode/linode/objbucket"
-<<<<<<< HEAD
-	"github.com/linode/terraform-provider-linode/linode/user"
-=======
-	"github.com/linode/terraform-provider-linode/linode/rdns"
->>>>>>> 10eff12f (creation)
 	"github.com/linode/terraform-provider-linode/linode/volume"
 )
 
@@ -147,7 +142,6 @@ func Provider() *schema.Provider {
 			"linode_nodebalancer_config":      nbconfig.Resource(),
 			"linode_object_storage_bucket":    objbucket.Resource(),
 			"linode_object_storage_object":    obj.Resource(),
-			"linode_rdns":                     rdns.Resource(),
 			"linode_volume":                   volume.Resource(),
 		},
 	}
