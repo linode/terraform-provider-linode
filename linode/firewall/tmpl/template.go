cd3f384f42665b85d076c9e3070a30825ff7ab3d
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2023-08-02 13:03:42 -0400

    Drop checks for non-zero firewall rules (#962)

--- linode/firewall/tmpl/template.go
+++ linode/firewall/tmpl/template.go
@@ -91,6 +91,13 @@ func NoIPv6(t *testing.T, label string) string {
 		})
 }
 
+func NoRules(t *testing.T, label string) string {
+	return acceptance.ExecuteTemplate(t,
+		"firewall_no_rules", TemplateData{
+			Label: label,
+		})
+}
+
 func DataBasic(t *testing.T, label, devicePrefix, region string) string {
 	return acceptance.ExecuteTemplate(t,
 		"firewall_data_basic", TemplateData{
