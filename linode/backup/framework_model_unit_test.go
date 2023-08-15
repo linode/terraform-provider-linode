04b5ebd2619e70e2f80bbea0fe4e614e80b44830
ykim-1 <ykim@akamai.com>
2023-08-15 16:34:45 -0600

    Add unit tests for linode/backup

---
+++ linode/backup/framework_model_unit_test.go
@@ -0,0 +1,48 @@
+package backup
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"testing"
+
+	"github.com/linode/linodego"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseBackups(t *testing.T) {
+	ctx := context.Background()
+
+	// Create mock data for InstanceSnapshot
+	mockSnapshot := &linodego.InstanceSnapshot{
+		ID:        1,
+		Label:     "Linode Snapshot Label",
+		Status:    "successful",
+		Type:      "snapshot",
+		Created:   nil,
+		Updated:   nil,
+		Finished:  nil,
+		Configs:   []string{"config1", "config2"},
+		Disks:     []*linodego.InstanceSnapshotDisk{}, // You can populate this with mock disk data
+		Available: true,
+	}
+
+	mockBackupSnapshotResponse := &linodego.InstanceBackupSnapshotResponse{
+		Current:    mockSnapshot,
+		InProgress: mockSnapshot,
+	}
+
+	mockBackups := &linodego.InstanceBackupsResponse{
+		Automatic: []*linodego.InstanceSnapshot{mockSnapshot},
+		Snapshot:  mockBackupSnapshotResponse,
+	}
+
+	linodeId := int64(123)
+
+	data := &DataSourceModel{}
+
+	diags := data.parseBackups(ctx, mockBackups, types.Int64Value(linodeId))
+
+	assert.False(t, diags.HasError(), "Expected no errors in diagnostics")
+
+	assert.Equal(t, types.Int64Value(linodeId), data.ID)
+}
