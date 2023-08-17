40dce176b938b9494915b6e11ed49417773040b5
ykim-1 <ykim@akamai.com>
2023-08-17 16:25:08 -0600

    more unit tests..

--- linode/nbnode/framework_models_unit_test.go
+++ linode/nbnode/framework_models_unit_test.go
@@ -1,1 +1,35 @@
 package nbnode
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/linode/linodego"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseNodeBalancerNode(t *testing.T) {
+	mockNode := &linodego.NodeBalancerNode{
+		ID:             54321,
+		Address:        "192.168.210.120:80",
+		Label:          "node54321",
+		Status:         "UP",
+		Weight:         50,
+		Mode:           "accept",
+		ConfigID:       4567,
+		NodeBalancerID: 12345,
+	}
+
+	data := &DataSourceModel{}
+
+	data.ParseNodeBalancerNode(mockNode)
+
+	assert.Equal(t, types.Int64Value(54321), data.ID)
+	assert.Equal(t, types.Int64Value(12345), data.NodeBalancerID)
+	assert.Equal(t, types.Int64Value(4567), data.ConfigID)
+	assert.Equal(t, types.StringValue("node54321"), data.Label)
+	assert.Equal(t, types.Int64Value(50), data.Weight)
+	assert.Equal(t, types.StringValue("accept"), data.Mode)
+	assert.Equal(t, types.StringValue("192.168.210.120:80"), data.Address)
+	assert.Equal(t, types.StringValue("UP"), data.Status)
+}
--- linode/region/framework_models_unit_test.go
+++ linode/region/framework_models_unit_test.go
@@ -1,1 +1,39 @@
 package region
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/linode/linodego"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseRegion(t *testing.T) {
+	region := &linodego.Region{
+		ID:           "us-east",
+		Country:      "us",
+		Capabilities: []string{"Linodes", "NodeBalancers", "Block Storage", "Object Storage"},
+		Status:       "ok",
+		Resolvers: linodego.RegionResolvers{
+			IPv4: "192.0.2.0,192.0.2.1",
+			IPv6: "2001:0db8::,2001:0db8::1",
+		},
+		Label: "Newark, NJ, USA",
+	}
+
+	model := &RegionModel{}
+
+	model.parseRegion(region)
+
+	assert.Equal(t, types.StringValue("us-east"), model.ID)
+	assert.Equal(t, types.StringValue("Newark, NJ, USA"), model.Label)
+	assert.Equal(t, types.StringValue("ok"), model.Status)
+	assert.Equal(t, types.StringValue("us"), model.Country)
+
+	for i, capability := range region.Capabilities {
+		assert.Equal(t, types.StringValue(capability), model.Capabilities[i])
+	}
+
+	assert.Equal(t, types.StringValue("192.0.2.0,192.0.2.1"), model.Resolvers[0].IPv4)
+	assert.Equal(t, types.StringValue("2001:0db8::,2001:0db8::1"), model.Resolvers[0].IPv6)
+}
