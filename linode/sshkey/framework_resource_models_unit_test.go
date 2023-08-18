92ccc3bf677a490f83072333a40bc55b67db85f2
ykim-1 <ykim@akamai.com>
2023-08-18 11:56:08 -0600

    all the unit tests

--- linode/sshkey/framework_resource_models_unit_test.go
+++ linode/sshkey/framework_resource_models_unit_test.go
@@ -1,1 +1,37 @@
 package sshkey
+
+import (
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/linode/linodego"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseConfiguredAttributes(t *testing.T) {
+	key := linodego.SSHKey{
+		Label:  "Test Key",
+		SSHKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC...",
+	}
+
+	rm := &ResourceModel{}
+	rm.parseConfiguredAttributes(&key)
+
+	assert.Equal(t, types.StringValue(key.Label), rm.Label)
+	assert.Equal(t, types.StringValue(key.SSHKey), rm.SSHKey)
+}
+
+func TestParseComputedAttributes(t *testing.T) {
+	created := time.Now()
+	key := linodego.SSHKey{
+		ID:      123,
+		Created: &created,
+	}
+
+	rm := &ResourceModel{}
+	rm.parseComputedAttributes(&key)
+
+	assert.Equal(t, types.Int64Value(int64(key.ID)), rm.ID)
+	assert.Equal(t, types.StringValue(created.Format(time.RFC3339)), rm.Created.StringValue)
+}
