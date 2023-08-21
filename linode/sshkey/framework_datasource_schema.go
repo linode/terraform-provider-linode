8dafc6d246a9f0e20bd42313266df7ed88735951
Ania Misiorek <139170033+amisiorek-akamai@users.noreply.github.com>
2023-08-21 10:22:37 -0400

    Add `linode_ssh_keys` data source (#981)
    * implementation
    
    * linting
    
    * changing for create

--- linode/sshkey/framework_datasource_schema.go
+++ linode/sshkey/framework_datasource_schema.go
@@ -2,25 +2,29 @@ package sshkey
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/linode/terraform-provider-linode/linode/helper/customtypes"
 )
 
-var frameworkDatasourceSchema = schema.Schema{
-	Attributes: map[string]schema.Attribute{
-		"label": schema.StringAttribute{
-			Description: "The label of the Linode SSH Key.",
-			Required:    true,
-		},
-		"ssh_key": schema.StringAttribute{
-			Description: "The public SSH Key, which is used to authenticate to the root user of the Linodes you deploy.",
-			Computed:    true,
-		},
-		"created": schema.StringAttribute{
-			Description: "The date this key was added.",
-			Computed:    true,
-		},
-		"id": schema.StringAttribute{
-			Description: "A unique identifier for this datasource.",
-			Optional:    true,
-		},
+var SSHKeyAttributes = map[string]schema.Attribute{
+	"label": schema.StringAttribute{
+		Description: "The label of the Linode SSH Key.",
+		Required:    true,
 	},
+	"ssh_key": schema.StringAttribute{
+		Description: "The public SSH Key, which is used to authenticate to the root user of the Linodes you deploy.",
+		Computed:    true,
+	},
+	"created": schema.StringAttribute{
+		CustomType:  customtypes.RFC3339TimeStringType{},
+		Description: "The date this key was added.",
+		Computed:    true,
+	},
+	"id": schema.StringAttribute{
+		Description: "A unique identifier for this datasource.",
+		Optional:    true,
+	},
+}
+
+var frameworkDatasourceSchema = schema.Schema{
+	Attributes: SSHKeyAttributes,
 }
