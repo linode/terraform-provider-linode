010a02d70dbf0028bb35b8bcf483b22b1a7c8653
Ye Chen <yechen@akamai.com>
2023-09-20 15:16:49 -0400

    use random test region

--- linode/vpc/resource_test.go
+++ linode/vpc/resource_test.go
@@ -26,9 +26,7 @@ func init() {
 		F:    sweep,
 	})
 
-	// testRegion = acceptance.GetRandomRegionWithCaps([]string{"VPCs"})
-
-	testRegion = "us-east" // VPC only available in certain regions currently
+	testRegion = acceptance.GetRandomRegionWithCaps([]string{"VPCs"})
 }
 
 func sweep(prefix string) error {
