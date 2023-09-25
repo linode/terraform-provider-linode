cb45ca8419628d33896421f9b5925c0f825a4993
Jacob Riddle <jriddle@akamai.com>
2023-09-25 17:52:40 +0000

    adding volumes list

---
+++ linode/volumes/framework_models.go
@@ -0,0 +1,41 @@
+package volumes
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/linode/linodego"
+	"github.com/linode/terraform-provider-linode/linode/helper/frameworkfilter"
+	"github.com/linode/terraform-provider-linode/linode/volume"
+)
+
+type VolumeModel struct {
+	ID      types.String                     `tfsdk:"id"`
+	Filters frameworkfilter.FiltersModelType `tfsdk:"filter"`
+	Order   types.String                     `tfsdk:"order"`
+	OrderBy types.String                     `tfsdk:"order_by"`
+	Volumes []volume.VolumeModel             `tfsdk:"volumes"`
+}
+
+func (data *VolumeModel) parseVolumes(
+	ctx context.Context,
+	client *linodego.Client,
+	volumes []linodego.Volume,
+) diag.Diagnostics {
+	result := make([]volume.VolumeModel, len(volumes))
+	for i := range volumes {
+		var mod volume.VolumeModel
+		diags := mod.ParseComputedAttributes(ctx, &volumes[i])
+		if diags.HasError() {
+			return diags
+		}
+		diags = mod.ParseNonComputedAttributes(ctx, &volumes[i])
+		if diags.HasError() {
+			return diags
+		}
+		result[i] = mod
+	}
+	data.Volumes = result
+	return nil
+}
