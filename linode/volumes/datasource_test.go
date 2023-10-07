9df49d5cd153e101d2d22bc09cd773c8732625c4
Zhiwei Liang <zliang@akamai.com>
2023-10-07 00:14:51 -0400

    Fix `TestAccDataSourceVolume_basic`

--- linode/volumes/datasource_test.go
+++ linode/volumes/datasource_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/linode/terraform-provider-linode/linode/acceptance"
-	"github.com/linode/terraform-provider-linode/linode/volume/tmpl"
+	"github.com/linode/terraform-provider-linode/linode/volumes/tmpl"
 )
 
 var testRegion string
