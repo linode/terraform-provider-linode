9ca2187876b1371a027f9cb2f6e0641da2ee3867
Zhiwei Liang <zliang@akamai.com>
2023-10-11 13:01:58 -0400

    replace SDKv2 test helpers with tf testing's

--- linode/databasemysql/datasource_test.go
+++ linode/databasemysql/datasource_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/linode/terraform-provider-linode/linode/acceptance"
 	"github.com/linode/terraform-provider-linode/linode/databasemysql/tmpl"
