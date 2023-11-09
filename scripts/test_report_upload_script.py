3cfeb406e4182c873de5a4751f566c597b9cfa4f
Youjung Kim <126618609+ykim-1@users.noreply.github.com>
2023-11-09 12:02:29 -0800

    test: change generated test report to acceptable xml for TOD (#1125)
    * debug gha
    
    * found the issue. missed replacing json with xml :(
    
    * Add steps to test_report_upload_script.py script to modify the generated xml report
    
    * reverted change
    
    * Update resource_test.go
    
    * Update resource_test.go
    
    * lint
    
    * oops forgot to remove one line
    
    * typo

--- scripts/test_report_upload_script.py
+++ scripts/test_report_upload_script.py
@@ -1,6 +1,7 @@
 import boto3
 import sys
 import os
+import xml.etree.ElementTree as ET
 from botocore.exceptions import NoCredentialsError
 
 ACCESS_KEY = os.environ.get('LINODE_CLI_OBJ_ACCESS_KEY')
@@ -13,6 +14,44 @@ linode_obj_config = {
     "endpoint_url": "https://us-southeast-1.linodeobjects.com",
     "region_name": "us-southeast-1",
 }
+
+def change_xml_report_to_tod_acceptable_version(file_name):
+    # Load the original XML file
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+
+    testsuites_element = root
+
+    # total
+    total_tests = int(testsuites_element.get('tests'))
+    total_failures = int(testsuites_element.get('failures'))
+    total_errors = int(testsuites_element.get('errors'))
+    total_skipped = int(testsuites_element.get('skipped'))
+
+    # Create a new <testsuites> element with aggregated values
+    new_testsuites = ET.Element("testsuites")
+    new_testsuites.set("tests", str(total_tests))
+    new_testsuites.set("failures", str(total_failures))
+    new_testsuites.set("errors", str(total_errors))
+    new_testsuites.set("skipped", str(total_skipped))
+
+    # Create a new <testsuite> element under <testsuites>
+    new_testsuite = ET.SubElement(new_testsuites, "testsuite", attrib=testsuites_element.attrib)
+
+    for testcase in root.findall('.//testcase'):
+        new_testcase = ET.SubElement(new_testsuite, "testcase", attrib=testcase.attrib)
+        for child in testcase:
+            new_testcase.append(child)
+
+    # Save the new XML to a file
+    try:
+        new_tree = ET.ElementTree(new_testsuites)
+        new_tree.write(file_name, encoding="UTF-8", xml_declaration=True)
+        print("XML content successfully over-written to " + file_name)
+
+    except Exception as e:
+        print("Error writing XML content:", str(e))
+
 
 
 def upload_to_linode_object_storage(file_name):
@@ -38,4 +77,5 @@ if __name__ == '__main__':
         print('Error: The provided file name is empty or invalid.')
         sys.exit(1)
 
+    change_xml_report_to_tod_acceptable_version(file_name)
     upload_to_linode_object_storage(file_name)
