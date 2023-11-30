7a708a9ce8af99cf8f961e079ea3173f16217f4d
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2023-11-30 15:33:12 -0500

    Migrate test helper import paths in VPC modules (#1165)

--- linode/vpcsubnet/datasource_test.go
+++ linode/vpcsubnet/datasource_test.go
@@ -5,7 +5,7 @@ package vpcsubnet_test
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/linode/terraform-provider-linode/v2/linode/acceptance"
 	"github.com/linode/terraform-provider-linode/v2/linode/vpcsubnet/tmpl"
