6ac9f2406efc20d98359b29de9d426d5479787d9
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2023-12-07 10:21:10 -0500

    fix: Add reconciliation logic for simplified `linode_domain_record` names (#1166)
    * Add reconciliation logic for shortened record names
    
    * oops

--- linode/domainrecord/resource.go
+++ linode/domainrecord/resource.go
@@ -77,7 +77,12 @@ func readResource(ctx context.Context, d *schema.ResourceData, meta interface{})
 		return diag.Errorf("Error finding the specified Linode DomainRecord: %s", err)
 	}
 
-	d.Set("name", record.Name)
+	reconciledName, err := reconcileName(ctx, client, d, record.Name)
+	if err != nil {
+		return diag.Errorf("failed to reconcile domain record name: %s", err)
+	}
+
+	d.Set("name", reconciledName)
 	d.Set("port", record.Port)
 	d.Set("priority", record.Priority)
 	d.Set("protocol", record.Protocol)
@@ -202,4 +207,28 @@ func deleteResource(ctx context.Context, d *schema.ResourceData, meta interface{
 func domainRecordTargetSuppressor(k, provisioned, declared string, d *schema.ResourceData) bool {
 	return len(strings.Split(declared, ".")) == 1 &&
 		strings.Contains(provisioned, declared)
+}
+
+// reconcileName handles cases where the user has specified their FQDN as a part of their
+// planned record name but the API trims the FQDN from the returned record name.
+func reconcileName(ctx context.Context, client linodego.Client, d *schema.ResourceData, apiName string) (string, error) {
+	plannedName, ok := d.GetOk("name")
+	if !ok {
+		return apiName, nil
+	}
+
+	domain, err := client.GetDomain(ctx, d.Get("domain_id").(int))
+	if err != nil {
+		return "", fmt.Errorf("failed to get parent domain: %w", err)
+	}
+
+	simplifiedPlanName := strings.TrimSuffix(strings.TrimSuffix(plannedName.(string), domain.Domain), ".")
+
+	// If the API response matches the planned name with the FQDN removed,
+	// return the planned value.
+	if apiName == simplifiedPlanName {
+		return plannedName.(string), nil
+	}
+
+	return apiName, nil
 }
