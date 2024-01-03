31301acc3d293cadc98c4666bcb8dc4ea40c04e0
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2024-01-03 13:36:54 -0500

    Cleanup unwanted comment (#1219)

--- linode/provider.go
+++ linode/provider.go
@@ -128,7 +128,7 @@ func Provider() *schema.Provider {
 			"linode_database_access_controls": databaseaccesscontrols.Resource(),
 			"linode_database_mysql":           databasemysql.Resource(),
 			"linode_database_postgresql":      databasepostgresql.Resource(),
-			"linode_domain":                   domain.Resource(), // f
+			"linode_domain":                   domain.Resource(),
 			"linode_domain_record":            domainrecord.Resource(),
 			"linode_firewall":                 firewall.Resource(),
 			"linode_image":                    image.Resource(),
