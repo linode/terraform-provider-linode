3a8d235c872f36853517d392d7ae0d81fa0630b2
Ye Chen <127243817+yec-akamai@users.noreply.github.com>
2024-01-25 14:21:22 -0500

    new: Add `linode_lke_clusters` data source (#1238)
    * lke clusters
    
    * fix integration
    
    * improve tests
    
    * reused controlPlane
    
    * Update linode/lkeclusters/datasource_test.go
    
    Co-authored-by: Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>

---
+++ linode/lkeclusters/tmpl/template.go
@@ -0,0 +1,24 @@
+package tmpl
+
+import (
+	"testing"
+
+	"github.com/linode/terraform-provider-linode/v2/linode/acceptance"
+)
+
+type TemplateData struct {
+	Label            string
+	K8sVersion       string
+	HighAvailability bool
+	Region           string
+}
+
+func DataBasic(t *testing.T, name, version, region string) string {
+	return acceptance.ExecuteTemplate(t,
+		"lke_clusters_data_basic", TemplateData{Label: name, K8sVersion: version, Region: region})
+}
+
+func DataFilter(t *testing.T, name, version, region string) string {
+	return acceptance.ExecuteTemplate(t,
+		"lke_clusters_data_filter", TemplateData{Label: name, K8sVersion: version, Region: region})
+}
