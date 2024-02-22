f19a955c43dbb650973ec46cdfc70cecb60faca0
ykim-1 <ykim@akamai.com>
2024-02-22 08:04:26 -0800

    make format

--- linode/ipv6range/framework_resource.go
+++ linode/ipv6range/framework_resource.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-log/tflog"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/linode/linodego"
 	"github.com/linode/terraform-provider-linode/v2/linode/helper"
 )
