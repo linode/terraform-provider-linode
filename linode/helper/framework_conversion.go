3f494508954ee9c5a0a92b1c0e63bb3300694354
Zhiwei Liang <zliang@akamai.com>
2024-03-26 00:50:23 -0400

    Add framework list helpers

--- linode/helper/framework_conversion.go
+++ linode/helper/framework_conversion.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
@@ -47,17 +48,110 @@ func StringSliceToFramework(val []string) []types.String {
 // StringSliceToFrameworkValueSlice converts the given string slice
 // into a framework-compatible slice of attr.Value.
 func StringSliceToFrameworkValueSlice(val []string) []attr.Value {
+	return GenericSliceToFramework(val, func(v string) attr.Value {
+		return types.StringValue(v)
+	})
+}
+
+type (
+	// A function that converts something into a framework-compatible attr.Value
+	// with a diag.Diagnostics for recording any error occurred.
+	FwValueConverter[T any, U attr.Value] func(T) (U, diag.Diagnostics)
+
+	// A function that converts something into a framework-compatible attr.Value
+	SafeFwValueConverter[T any, U attr.Value] func(T) U
+)
+
+// Returns a converter that echos the framework value without any conversion
+func FwValueEchoConverter() FwValueConverter[attr.Value, attr.Value] {
+	return func(v attr.Value) (attr.Value, diag.Diagnostics) {
+		return v, nil
+	}
+}
+
+// Returns a converter that echos the framework value without any
+// conversion without diags.Diagnostics support
+func SafeFwValueEchoConverter() SafeFwValueConverter[attr.Value, attr.Value] {
+	return func(v attr.Value) attr.Value { return v }
+}
+
+// Returns a converter that cast the result of the passed in converter
+// to be base framework value (aka attr.Value)
+func GetBaseFwValueConverter[T any, U attr.Value](c FwValueConverter[T, U]) FwValueConverter[T, attr.Value] {
+	return func(v T) (attr.Value, diag.Diagnostics) {
+		return c(v)
+	}
+}
+
+// Returns a converter that cast the result of the passed in converter to
+// be base framework value (aka attr.Value) without diags.Diagnostics support
+func GetBaseSafeFwValueConverter[T any, U attr.Value](c FwValueConverter[T, U]) FwValueConverter[T, attr.Value] {
+	return func(v T) (attr.Value, diag.Diagnostics) {
+		return c(v)
+	}
+}
+
+// GenericSliceToFramework converts the given generic slice
+// into a framework-compatible slice of attr.Value.
+func GenericSliceToFramework[T any, U attr.Value](
+	val []T, converter SafeFwValueConverter[T, U],
+) []U {
 	if val == nil {
 		return nil
 	}
 
-	result := make([]attr.Value, len(val))
+	result := make([]U, len(val))
 
 	for i, v := range val {
-		result[i] = types.StringValue(v)
+		resultVal := converter(v)
+		result[i] = resultVal
 	}
 
 	return result
+}
+
+// GenericSliceToFrameworkWithDiags converts the given generic slice
+// into a framework-compatible slice of attr.Value with a converter and
+// diag.Diagnostics for recording any error occurred.
+func GenericSliceToFrameworkWithDiags[T any, U attr.Value](
+	val []T, converter FwValueConverter[T, U], diags *diag.Diagnostics,
+) []U {
+	if val == nil {
+		return nil
+	}
+
+	result := make([]U, len(val))
+
+	for i, v := range val {
+		resultVal, d := converter(v)
+		if d.HasError() {
+			diags.Append(d...)
+			return nil
+		}
+		result[i] = resultVal
+	}
+
+	return result
+}
+
+// GenericSliceToList converts the given generic slice
+// into a framework-compatible value of types.List with a FwValueConverter.
+func GenericSliceToList[T any, V attr.Value](
+	val []T, elementType attr.Type, converter FwValueConverter[T, V], diags *diag.Diagnostics,
+) types.List {
+	if val == nil {
+		return types.ListNull(elementType)
+	}
+
+	result := GenericSliceToFrameworkWithDiags(val, GetBaseFwValueConverter(converter), diags)
+	if diags.HasError() {
+		return types.ListNull(elementType)
+	}
+
+	listResult, newDiags := types.ListValue(elementType, result)
+	diags.Append(newDiags...)
+
+	return listResult
 }
 
 // FrameworkSliceToString converts the given Framework slice
@@ -79,15 +173,7 @@ func FrameworkSliceToString(val []types.String) []string {
 // IntSliceToFramework converts the given int slice
 // into a framework-compatible slice of types.String.
 func IntSliceToFramework(val []int) []types.Int64 {
-	if val == nil {
-		return nil
-	}
-
-	result := make([]types.Int64, len(val))
-
-	for i, v := range val {
-		result[i] = types.Int64Value(int64(v))
-	}
-
-	return result
+	return GenericSliceToFramework(val, func(v int) types.Int64 {
+		return types.Int64Value(int64(v))
+	})
 }
