2a9f67928ea55f1c110ebc422259d5528a0358b0
Lena Garber <lgarber@akamai.com>
2024-05-01 16:21:26 -0400

    Update unit tests according to model changes

--- linode/placementgroup/framework_models_unit_test.go
+++ linode/placementgroup/framework_models_unit_test.go
@@ -3,10 +3,11 @@
 package placementgroup
 
 import (
+	"context"
 	"testing"
 
 	"github.com/linode/linodego"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFlattenPGModel(t *testing.T) {
@@ -30,19 +31,24 @@ func TestFlattenPGModel(t *testing.T) {
 	}
 
 	model := PlacementGroupModel{}
-	model.FlattenPlacementGroup(&pg, false)
+	model.FlattenPlacementGroup(context.Background(), &pg, false)
 
-	assert.Equal(t, "123", model.ID.ValueString())
-	assert.Equal(t, "test", model.Label.ValueString())
-	assert.Equal(t, "us-mia", model.Region.ValueString())
-	assert.Equal(t, string(linodego.AffinityTypeAntiAffinityLocal), model.AffinityType.ValueString())
-	assert.Equal(t, false, model.IsStrict.ValueBool())
+	require.Equal(t, "123", model.ID.ValueString())
+	require.Equal(t, "test", model.Label.ValueString())
+	require.Equal(t, "us-mia", model.Region.ValueString())
+	require.Equal(t, string(linodego.AffinityTypeAntiAffinityLocal), model.AffinityType.ValueString())
+	require.Equal(t, false, model.IsStrict.ValueBool())
 
-	assert.Equal(t, false, model.IsCompliant.ValueBool())
+	require.Equal(t, false, model.IsCompliant.ValueBool())
 
-	assert.Equal(t, int64(123), model.Members[0].LinodeID.ValueInt64())
-	assert.Equal(t, false, model.Members[0].IsCompliant.ValueBool())
+	members := make([]PlacementGroupMemberModel, 0)
 
-	assert.Equal(t, int64(456), model.Members[1].LinodeID.ValueInt64())
-	assert.Equal(t, true, model.Members[1].IsCompliant.ValueBool())
+	d := model.Members.ElementsAs(context.Background(), &members, false)
+	require.False(t, d.HasError(), d.Errors())
+
+	require.Equal(t, int64(123), members[0].LinodeID.ValueInt64())
+	require.Equal(t, false, members[0].IsCompliant.ValueBool())
+
+	require.Equal(t, int64(456), members[1].LinodeID.ValueInt64())
+	require.Equal(t, true, members[1].IsCompliant.ValueBool())
 }
