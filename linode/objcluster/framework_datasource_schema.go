82670fd8f108be75de4865104035fa2f88b16966
Zhiwei Liang <zliang@akamai.com>
2024-06-06 15:38:22 -0400

    Deprecates linode_object_storage_cluster data source

--- linode/objcluster/framework_datasource_schema.go
+++ linode/objcluster/framework_datasource_schema.go
@@ -5,6 +5,11 @@ import (
 )
 
 var frameworkDatasourceSchema = schema.Schema{
+	DeprecationMessage: "'linode_object_storage_cluster' data source " +
+		"has been deprecated because it uses a deprecated View Cluster " +
+		"API. In the future, 'region' will be preferred over 'cluster' " +
+		"for the request body of many other Object Storage related API " +
+		"endpoints because there may be multiple clusters per region.",
 	Attributes: map[string]schema.Attribute{
 		"id": schema.StringAttribute{
 			Description: "The unique ID of this Cluster.",
