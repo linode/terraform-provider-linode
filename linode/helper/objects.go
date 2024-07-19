74e741d7e55cdb862615a86b458d2e10bc4a1856
ezilber-akamai <ezilber@akamai.com>
2024-07-19 09:16:07 -0400

    Updated helper to use more general error message

--- linode/helper/objects.go
+++ linode/helper/objects.go
@@ -105,7 +105,7 @@ func IsBucketNotFoundError(err error) bool {
 		context.Background(),
 		fmt.Sprintf("received an error: %s, checking whether it's a bucket not found error", err),
 	)
-	return strings.Contains(err.Error(), "[400] [bucket_access[0].bucket_name] Bucket not found")
+	return strings.Contains(err.Error(), "Bucket not found")
 }
 
 // Purge all objects, wiping out all versions and delete markers for versioned objects.
