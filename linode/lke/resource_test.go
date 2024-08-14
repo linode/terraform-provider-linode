ab00c712d4f3fa18e623d6a29334d5861646b43b
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2024-08-14 15:09:22 -0400

    Refactor `parseLKEAttributes` function and reduce size of test linode instances for LKE (#1546)
    * Refactor parseLKEAttributes function for LKE data source
    
    * use cheaper instances for testing

--- linode/lke/resource_test.go
+++ linode/lke/resource_test.go
@@ -183,7 +183,7 @@ func TestAccResourceLKECluster_basic_smoke(t *testing.T) {
 						resource.TestCheckResourceAttr(resourceClusterName, "status", "ready"),
 						resource.TestCheckResourceAttr(resourceClusterName, "tags.#", "1"),
 						resource.TestCheckResourceAttr(resourceClusterName, "pool.#", "1"),
-						resource.TestCheckResourceAttr(resourceClusterName, "pool.0.type", "g6-standard-2"),
+						resource.TestCheckResourceAttr(resourceClusterName, "pool.0.type", "g6-standard-1"),
 						resource.TestCheckResourceAttr(resourceClusterName, "pool.0.count", "3"),
 						resource.TestCheckResourceAttrSet(resourceClusterName, "pool.0.disk_encryption"),
 						resource.TestCheckResourceAttr(resourceClusterName, "pool.0.nodes.#", "3"),
