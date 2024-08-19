b38421bbdf64390cd490d5735c717a22afcaecd2
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2024-08-19 12:46:51 -0400

    Add taints and labels support to node pool resource (#1537)
    * Add taints and labels support to node pool resource
    
    * Revert unwanted changes
    
    * Add description for taint block
    
    * Add docs
    
    * gofumpt
    
    * Fix unit testing

--- linode/helper/framework_schema.go
+++ linode/helper/framework_schema.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/defaults"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setdefault"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
@@ -12,6 +13,15 @@ func EmptySetDefault(elemType attr.Type) defaults.Set {
 		types.SetValueMust(
 			elemType,
 			[]attr.Value{},
+		),
+	)
+}
+
+func EmptyMapDefault(elemType attr.Type) defaults.Map {
+	return mapdefault.StaticValue(
+		types.MapValueMust(
+			elemType,
+			map[string]attr.Value{},
 		),
 	)
 }
