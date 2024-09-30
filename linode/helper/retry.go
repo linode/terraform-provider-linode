43d7e46f16cd080a842f9a907daff13564a0cd09
Lena Garber <lgarber@akamai.com>
2024-09-30 14:30:53 -0400

    Implement changes for resty removal

--- linode/helper/retry.go
+++ linode/helper/retry.go
@@ -2,15 +2,15 @@ package helper
 
 import (
 	"log"
+	"net/http"
 	"net/url"
 	"regexp"
 
-	"github.com/go-resty/resty/v2"
 	"github.com/linode/linodego"
 )
 
 // Workaround for intermittent 5xx errors when retrieving a database from the API
-func Database502Retry() func(response *resty.Response, err error) bool {
+func Database502Retry() func(response *http.Response, err error) bool {
 	databaseGetRegex, err := regexp.Compile("[A-Za-z0-9]+/databases/[a-z]+/instances/[0-9]+")
 	if err != nil {
 		log.Fatal(err)
@@ -18,7 +18,7 @@ func Database502Retry() func(response *resty.Response, err error) bool {
 	return GenericRetryCondition(500, databaseGetRegex)
 }
 
-func LinodeInstance500Retry() func(response *resty.Response, err error) bool {
+func LinodeInstance500Retry() func(response *http.Response, err error) bool {
 	linodeGetRegex, err := regexp.Compile("linode/instances/[0-9]+/ips+")
 	if err != nil {
 		log.Fatal(err)
@@ -27,7 +27,7 @@ func LinodeInstance500Retry() func(response *resty.Response, err error) bool {
 }
 
 // ImageUpload500Retry for [500] error when uploading an image
-func ImageUpload500Retry() func(response *resty.Response, err error) bool {
+func ImageUpload500Retry() func(response *http.Response, err error) bool {
 	ImageUpload, err := regexp.Compile("images/upload")
 	if err != nil {
 		log.Fatal(err)
@@ -35,13 +35,13 @@ func ImageUpload500Retry() func(response *resty.Response, err error) bool {
 	return GenericRetryCondition(500, ImageUpload)
 }
 
-func GenericRetryCondition(statusCode int, pathPattern *regexp.Regexp) func(response *resty.Response, err error) bool {
-	return func(response *resty.Response, _ error) bool {
-		if response.StatusCode() != statusCode || response.Request == nil {
+func GenericRetryCondition(statusCode int, pathPattern *regexp.Regexp) func(response *http.Response, err error) bool {
+	return func(response *http.Response, _ error) bool {
+		if response.StatusCode != statusCode || response.Request == nil {
 			return false
 		}
 
-		requestURL, err := url.ParseRequestURI(response.Request.URL)
+		requestURL, err := url.ParseRequestURI(response.Request.RequestURI)
 		if err != nil {
 			log.Printf("[WARN] failed to parse request URL: %s", err)
 			return false
