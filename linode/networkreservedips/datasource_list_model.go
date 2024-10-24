82e8f7bf03a3662cecd4275032f4972fa1a23a2d
Anirudh Jagadish <ajagadis@akamai.com>
2024-10-24 15:49:31 -0400

    deleting accidental commit changes

--- linode/networkreservedips/datasource_list_model.go
+++
@@ -1,9 +0,0 @@
-package networkreservedips
-
-import (
-	"github.com/hashicorp/terraform-plugin-framework/types"
-)
-
-type DataSourceListModel struct {
-	ReservedIPs types.List `tfsdk:"reserved_ips"`
-}
