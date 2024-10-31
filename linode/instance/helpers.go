288c27d7bc56df40a33aab76a8f8edae07539e81
Zhiwei Liang <zliang@akamai.com>
2024-10-31 16:07:23 -0400

    Support upgrade attribute for DC migration

--- linode/instance/helpers.go
+++ linode/instance/helpers.go
@@ -1063,6 +1063,7 @@ func applyInstanceMigration(
 	client *linodego.Client,
 	instance *linodego.Instance,
 	targetRegion string,
+	upgrade bool,
 ) (*linodego.Instance, error) {
 	migrationType := linodego.InstanceMigrationType(
 		d.Get("migration_type").(string),
@@ -1081,8 +1082,9 @@ func applyInstanceMigration(
 	}
 
 	migrateOpts := linodego.InstanceMigrateOptions{
-		Region: targetRegion,
-		Type:   migrationType,
+		Region:  targetRegion,
+		Type:    migrationType,
+		Upgrade: &upgrade,
 	}
 
 	tflog.Debug(ctx, "client.MigrateInstance(...)", map[string]any{
