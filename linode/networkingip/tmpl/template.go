e82cc76956f43279191714c2863aa8c125198bb2
Anirudh Jagadish <ajagadis@akamai.com>
2024-10-31 12:11:34 -0400

    Added test for reserved IP creation and allocation

--- linode/networkingip/tmpl/template.go
+++ linode/networkingip/tmpl/template.go
@@ -15,3 +15,38 @@ func DataBasic(t *testing.T, label, region string) string {
 	return acceptance.ExecuteTemplate(t,
 		"networking_ip_data_basic", TemplateData{Label: label, Region: region})
 }
+
+func NetworkingIPReservedAssigned(t *testing.T, label string, region string) string {
+	return acceptance.ExecuteTemplate(t,
+		"networking_ip_reserved_assigned",
+		TemplateData{
+			Label:  label,
+			Region: region,
+		})
+}
+
+func NetworkingIPReserved(t *testing.T, region string) string {
+	return acceptance.ExecuteTemplate(t,
+		"networking_ip_reserved",
+		TemplateData{
+			Region: region,
+		})
+}
+
+func NetworkingIPEphemeral(t *testing.T, label string, region string) string {
+	return acceptance.ExecuteTemplate(t,
+		"networking_ip_ephemeral",
+		TemplateData{
+			Label:  label,
+			Region: region,
+		})
+}
+
+func NetworkingIPEphemeralAssigned(t *testing.T, label string, region string) string {
+	return acceptance.ExecuteTemplate(t,
+		"networking_ip_ephemeral_assigned",
+		TemplateData{
+			Label:  label,
+			Region: region,
+		})
+}
