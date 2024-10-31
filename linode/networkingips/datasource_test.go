3fa103a5d1475bc4516437cdbdba50d2b867d13c
Anirudh Jagadish <ajagadis@akamai.com>
2024-10-30 21:18:49 -0400

    Added test for filterig list of ip address on the reserved field

--- linode/networkingips/datasource_test.go
+++ linode/networkingips/datasource_test.go
@@ -23,6 +23,35 @@ func init() {
 	testRegion = region
 }
 
+func TestAccDataSourceNetworkingIP_list(t *testing.T) {
+	t.Parallel()
+
+	dataResourceName := "data.linode_networking_ip.list"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acceptance.PreCheck(t) },
+		ProtoV5ProviderFactories: acceptance.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: tmpl.DataList(t),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataResourceName, "ip_addresses.#"),
+					resource.TestCheckResourceAttrSet(dataResourceName, "ip_addresses.0.address"),
+					resource.TestCheckResourceAttrSet(dataResourceName, "ip_addresses.0.linode_id"),
+					resource.TestCheckResourceAttrSet(dataResourceName, "ip_addresses.0.region"),
+					resource.TestMatchResourceAttr(dataResourceName, "ip_addresses.0.gateway", regexp.MustCompile(`\.1$`)),
+					resource.TestCheckResourceAttr(dataResourceName, "ip_addresses.0.type", "ipv4"),
+					resource.TestCheckResourceAttr(dataResourceName, "ip_addresses.0.public", "true"),
+					resource.TestCheckResourceAttr(dataResourceName, "ip_addresses.0.prefix", "24"),
+					resource.TestMatchResourceAttr(dataResourceName, "ip_addresses.0.rdns", regexp.MustCompile(`.ip.linodeusercontent.com$`)),
+					resource.TestCheckResourceAttrSet(dataResourceName, "ip_addresses.0.subnet_mask"),
+					resource.TestCheckResourceAttrSet(dataResourceName, "ip_addresses.0.reserved"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDataSourceNetworkingIP_filterReserved(t *testing.T) {
 	t.Parallel()
 
---
+++ linode/networkingips/tmpl/template.go
@@ -0,0 +1,15 @@
+package tmpl
+
+import (
+	"testing"
+
+	"github.com/linode/terraform-provider-linode/v2/linode/acceptance"
+)
+
+func DataList(t *testing.T) string {
+	return acceptance.ExecuteTemplate(t, "networking_ip_data_list", nil)
+}
+
+func DataFilterReserved(t *testing.T) string {
+	return acceptance.ExecuteTemplate(t, "networking_ip_data_filtered", nil)
+}
