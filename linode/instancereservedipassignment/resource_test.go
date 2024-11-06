27fbc90d712d58f483292073b642e6836e817489
Anirudh Jagadish <ajagadis@akamai.com>
2024-11-06 12:44:38 -0500

    uncommented the func to generate region

--- linode/instancereservedipassignment/resource_test.go
+++ linode/instancereservedipassignment/resource_test.go
@@ -3,6 +3,7 @@
 package instancereservedipassignment_test
 
 import (
+	"log"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
@@ -17,10 +18,10 @@ const testInstanceIPResName = "linode_reserved_ip_assignment.test"
 var testRegion string
 
 func init() {
-	// region, err := acceptance.GetRandomRegionWithCaps(nil, "core")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	region, err := acceptance.GetRandomRegionWithCaps(nil, "core")
+	if err != nil {
+		log.Fatal(err)
+	}
 	region := "us-east"
 	testRegion = region
 }
@@ -31,7 +32,7 @@ func TestAccInstanceIP_addReservedIP(t *testing.T) {
 
 	var instance linodego.Instance
 	name := acctest.RandomWithPrefix("tf_test")
-	reservedIP := "50.116.48.72" // Replace with your actual reserved IP address
+	reservedIP := "50.116.48.7223" // Replace with your actual reserved IP address
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acceptance.PreCheck(t) },
 		ProtoV5ProviderFactories: acceptance.ProtoV5ProviderFactories,
