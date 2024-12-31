38d320a7511398d9097fbdc54efad4dda19a48f9
Lena Garber <lgarber@akamai.com>
2024-12-31 14:05:35 -0500

    Drop allow_list default

--- linode/databasepostgresqlv2/framework_resource_schema.go
+++ linode/databasepostgresqlv2/framework_resource_schema.go
@@ -14,14 +14,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/linode/terraform-provider-linode/v2/linode/helper"
 )
 
 var (
-	allowListDefault = helper.FrameworkMust(
-		types.SetValue(types.StringType, []attr.Value{types.StringValue("0.0.0.0/0")}),
-	)
-
 	updatesAttributes = map[string]attr.Type{
 		"day_of_week": types.Int64Type,
 		"duration":    types.Int64Type,
