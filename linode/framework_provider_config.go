eb04e2e6fc04f97e2673f69d1994ce76946a7106
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2025-05-19 11:58:10 -0400

    Update linode/framework_provider_config.go
    Co-authored-by: Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>

--- linode/framework_provider_config.go
+++ linode/framework_provider_config.go
@@ -58,7 +58,7 @@ func (fp *FrameworkProvider) Configure(
 		resp.Diagnostics.AddWarning(
 			"Support for Terraform 0.x is deprecated",
 			"Please upgrade to Terraform 1.x or later. "+
-				"Support for Terraform 0.x will be removed in a future version of the provider.",
+				"Support for Terraform 0.x will be removed in a future version of this provider.",
 		)
 	}
 
