ecd924dcc68b9a37b15223a8a1e1a9dc483f6681
Zhiwei Liang <zliang@akamai.com>
2025-06-26 16:24:21 -0400

    Migrate to nested object

--- linode/firewallsettings/framework_datasource_schema.go
+++ linode/firewallsettings/framework_datasource_schema.go
@@ -1,26 +1,20 @@
 package firewallsettings
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 )
-
-var DefaultFirewallIDs = types.ObjectType{
-	AttrTypes: map[string]attr.Type{
-		"linode":           types.Int64Type,
-		"nodebalancer":     types.Int64Type,
-		"public_interface": types.Int64Type,
-		"vpc_interface":    types.Int64Type,
-	},
-}
 
 var frameworkDatasourceSchema = schema.Schema{
 	Attributes: map[string]schema.Attribute{
-		"default_firewall_ids": schema.ObjectAttribute{
-			Description:    "The default firewall ID for a linode, nodebalancer, public_interface, or vpc_interface.",
-			Computed:       true,
-			AttributeTypes: DefaultFirewallIDs.AttrTypes,
+		"default_firewall_ids": schema.SingleNestedAttribute{
+			Computed:    true,
+			Description: "The default firewall ID for a linode, nodebalancer, public_interface, or vpc_interface.",
+			Attributes: map[string]schema.Attribute{
+				"linode":           schema.Int64Attribute{Computed: true},
+				"nodebalancer":     schema.Int64Attribute{Computed: true},
+				"public_interface": schema.Int64Attribute{Computed: true},
+				"vpc_interface":    schema.Int64Attribute{Computed: true},
+			},
 		},
 	},
 }
