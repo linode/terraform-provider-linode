b13a8d634eafa75f9ed1994bce9b9abf58826ba7
Zhiwei Liang <zliang@akamai.com>
2025-07-10 01:46:21 -0400

    Add doc

--- linode/firewallsettings/framework_resource_schema.go
+++ linode/firewallsettings/framework_resource_schema.go
@@ -13,6 +13,7 @@ var FrameworkResourceSchema = schema.Schema{
 			Description: "The default firewall ID for a linode, nodebalancer, public_interface, or vpc_interface.",
 			Attributes: map[string]schema.Attribute{
 				"linode": schema.Int64Attribute{
+					Description: "The Linode's default firewall.",
 					PlanModifiers: []planmodifier.Int64{
 						int64planmodifier.UseStateForUnknown(),
 					},
@@ -20,6 +21,7 @@ var FrameworkResourceSchema = schema.Schema{
 					Computed: true,
 				},
 				"nodebalancer": schema.Int64Attribute{
+					Description: "The NodeBalancer's default firewall.",
 					PlanModifiers: []planmodifier.Int64{
 						int64planmodifier.UseStateForUnknown(),
 					},
@@ -27,6 +29,7 @@ var FrameworkResourceSchema = schema.Schema{
 					Computed: true,
 				},
 				"public_interface": schema.Int64Attribute{
+					Description: "The public interface's default firewall.",
 					PlanModifiers: []planmodifier.Int64{
 						int64planmodifier.UseStateForUnknown(),
 					},
@@ -34,6 +37,7 @@ var FrameworkResourceSchema = schema.Schema{
 					Computed: true,
 				},
 				"vpc_interface": schema.Int64Attribute{
+					Description: "The VPC interface's default firewall.",
 					PlanModifiers: []planmodifier.Int64{
 						int64planmodifier.UseStateForUnknown(),
 					},
