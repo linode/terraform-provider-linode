46ff63795d47022027c6c0b8d54116092a47bbd1
rpotla <rpotla@akamai.com>
2025-08-26 15:27:29 -0400

    fix integration tests

--- linode/lkenodepool/framework_resource_schema.go
+++ linode/lkenodepool/framework_resource_schema.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -37,9 +38,8 @@ var resourceSchema = schema.Schema{
 		"label": schema.StringAttribute{
 			Description: "The label of the Node Pool.",
 			Optional:    true,
-			PlanModifiers: []planmodifier.String{
-				stringplanmodifier.UseStateForUnknown(),
-			},
+			Default:     stringdefault.StaticString(""),
+			Computed:    true,
 		},
 		"node_count": schema.Int64Attribute{
 			Validators: []validator.Int64{
