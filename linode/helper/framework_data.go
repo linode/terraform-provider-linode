6f17947a34ca48e5093a029efecfe50a56c9f72b
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2025-09-11 12:17:14 -0400

    Add nested object update helper (#2002)
    * Implement nested object update helper
    
    * Add isNull return (pass by ptr)
    
    * FIx
    
    * Minor change

--- linode/helper/framework_data.go
+++ linode/helper/framework_data.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 func KeepOrUpdateString(original types.String, updated string, preserveKnown bool) types.String {
@@ -102,4 +103,89 @@ func KeepOrUpdateValue[T attr.Value](original T, updated T, preserveKnown bool) 
 		return original
 	}
 	return updated
+}
+
+// KeepOrUpdateSingleNestedAttribute is a convenience wrapper to keep or update a single nested attribute.
+// Should only use for the single nested object at root level. For multi-layer nested object, use
+// KeepOrUpdateSingleNestedAttributeWithTypes instead.
+func KeepOrUpdateSingleNestedAttribute[T any](
+	ctx context.Context,
+	original types.Object,
+	preserveKnown bool,
+	diags *diag.Diagnostics,
+	flatten func(*T, *bool, bool, *diag.Diagnostics),
+) *types.Object {
+	return KeepOrUpdateSingleNestedAttributeWithTypes(
+		ctx, original, original.AttributeTypes(ctx), preserveKnown, diags, flatten,
+	)
+}
+
+// FlattenNestedObjectFunc flattens linodego structs into their corresponding Terraform framework model structs.
+//
+// Set `isNull` to true if the nested object should be nullified.
+//
+// For any collection attribute (set, list, map) with a null value, override it with a null value with the
+// corresponding element type (e.g., types.SetNull(types.StringType)). This ensures the framework can determine
+// the element type when setting the attribute in the state. This is necessary because when the original nested
+// object is null or unknown, the KeepOrUpdateSingleNestedAttributeWithTypes function cannot provide element
+// type information for the attributes within.
+type FlattenNestedObjectFunc[T any] func(model *T, isNull *bool, preserveKnown bool, diags *diag.Diagnostics)
+
+// This function is necessary when explicit attributes are needed for flatten the `original`
+// nested object.
+//
+// In some cases `original` won't contain the type of its attributes. For example, a
+// double nested object (nested object in another nested object) in a model; when the
+// parent nested object is null or unknown, `object.As` won't put the attributes into
+// the child nested object. Passing explicit attributeTypes will then be necessary.
+//
+// Checkout the corresponding unit tests for more details.
+func KeepOrUpdateSingleNestedAttributeWithTypes[T any](
+	ctx context.Context,
+	original types.Object,
+	attributeTypes map[string]attr.Type,
+	preserveKnown bool,
+	diags *diag.Diagnostics,
+	flatten FlattenNestedObjectFunc[T],
+) *types.Object {
+	if preserveKnown && original.IsNull() {
+		return &original
+	}
+
+	var attrModel T
+
+	if !original.IsUnknown() && !original.IsNull() {
+		diags.Append(
+			original.As(ctx, &attrModel, basetypes.ObjectAsOptions{
+				UnhandledNullAsEmpty:    false,
+				UnhandledUnknownAsEmpty: false,
+			})...,
+		)
+		if diags.HasError() {
+			return nil
+		}
+	}
+
+	preserveKnown = preserveKnown && !original.IsUnknown()
+	isNull := false
+
+	flatten(&attrModel, &isNull, preserveKnown, diags)
+
+	var updated types.Object
+
+	// Only setting it to null when not preserving known.
+	// When known values are preserved, it's the flatten function's
+	// responsibility to handle the values of the nested attributes
+	if isNull && !preserveKnown {
+		updated = types.ObjectNull(attributeTypes)
+	} else {
+		var newDiags diag.Diagnostics
+		updated, newDiags = types.ObjectValueFrom(ctx, attributeTypes, attrModel)
+		diags.Append(newDiags...)
+		if diags.HasError() {
+			return nil
+		}
+	}
+
+	return &updated
 }
