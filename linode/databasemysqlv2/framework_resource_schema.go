f6106c59fd60e777429af87de9966852d0abd92d
Zhiwei Liang <zliang@akamai.com>
2026-02-01 00:25:13 -0500

    Centralized plan modifier for dbaas host attributes

--- linode/databasemysqlv2/framework_resource_schema.go
+++ linode/databasemysqlv2/framework_resource_schema.go
@@ -8,7 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/float64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
@@ -152,14 +151,9 @@ var frameworkResourceSchema = schema.Schema{
 			},
 		},
 		"host_primary": schema.StringAttribute{
-			Description: "The primary host for the Managed Database.",
-			Computed:    true,
-			PlanModifiers: []planmodifier.String{
-				stringplanmodifiers.UseStateForUnknownUnlessTheseChanged(
-					path.MatchRoot("private_network"),
-					path.MatchRoot("type"),
-				),
-			},
+			Description:   "The primary host for the Managed Database.",
+			Computed:      true,
+			PlanModifiers: databaseshared.HostStringPlanModifiers,
 		},
 		"host_secondary": schema.StringAttribute{
 			Description:        "The secondary/private host for the Managed Database.",
@@ -167,14 +161,9 @@ var frameworkResourceSchema = schema.Schema{
 			DeprecationMessage: "Use host_standby instead.",
 		},
 		"host_standby": schema.StringAttribute{
-			Description: "The standby host for the Managed Database.",
-			Computed:    true,
-			PlanModifiers: []planmodifier.String{
-				stringplanmodifiers.UseStateForUnknownUnlessTheseChanged(
-					path.MatchRoot("private_network"),
-					path.MatchRoot("type"),
-				),
-			},
+			Description:   "The standby host for the Managed Database.",
+			Computed:      true,
+			PlanModifiers: databaseshared.HostStringPlanModifiers,
 		},
 		"members": schema.MapAttribute{
 			ElementType: types.StringType,
