80639567288dd459f534be7f246ea73379609d55
Dawid Dzhafarov <ddzhafar@akamai.com>
2026-03-16 10:03:32 +0100

    TPT-4110 Add integration build tags

--- linode/regionsvpcavailability/datasource_test.go
+++ linode/regionsvpcavailability/datasource_test.go
@@ -1,3 +1,5 @@
+//go:build integration || regionsvpcavailability
+
 package regionsvpcavailability_test
 
 import (
--- linode/regionvpcavailability/datasource_test.go
+++ linode/regionvpcavailability/datasource_test.go
@@ -1,3 +1,5 @@
+//go:build integration || regionvpcavailability
+
 package regionvpcavailability_test
 
 import (
