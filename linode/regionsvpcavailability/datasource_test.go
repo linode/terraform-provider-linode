32cbcdb9e519d569cbe4d8a9499dd32afead2ce7
Dawid <56120592+dawiddzhafarov@users.noreply.github.com>
2026-03-17 08:08:40 +0100

    TPT-4110 Add datasources for region vpc availability and regions vpc availability (#2246)
    * Add datasources for region vpc availability and regions vpc availability
    
    * Format .md files
    
    * Adress copilot suggestions
    
    * Bump linodego version to use the latest functionality
    
    * Update test data
    
    * TPT-4110 Add integration build tags
    
    * TPT-4110 Generalize test assertions

---
+++ linode/regionsvpcavailability/datasource_test.go
@@ -0,0 +1,34 @@
+//go:build integration || regionsvpcavailability
+
+package regionsvpcavailability_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/linode/terraform-provider-linode/v3/linode/acceptance"
+	"github.com/linode/terraform-provider-linode/v3/linode/regionsvpcavailability/tmpl"
+)
+
+func TestAccDataSourceRegionsVPCAvailability_basic(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "data.linode_regions_vpc_availability.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acceptance.PreCheck(t) },
+		ProtoV6ProviderFactories: acceptance.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: tmpl.DataBasic(t),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("id"), knownvalue.NotNull()),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("regions_vpc_availability"), knownvalue.NotNull()),
+				},
+			},
+		},
+	})
+}
