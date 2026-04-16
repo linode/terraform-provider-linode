ed89822e979a7b52a4a5b40a372acd9d1d0a28d9
Michal Wojcik <miwojci@akamai.com>
2026-04-16 16:55:53 +0200

    TPT-4292: terraform: Implement linode_reserved_ip resource

--- linode/reservedip/tmpl/template.go
+++ linode/reservedip/tmpl/template.go
@@ -11,14 +11,14 @@ type TemplateData struct {
 	Tags   []string
 }
 
-func Basic(t *testing.T, region string) string {
+func Basic(t testing.TB, region string) string {
 	return acceptance.ExecuteTemplate(t,
 		"reserved_ip_basic", TemplateData{
 			Region: region,
 		})
 }
 
-func WithTags(t *testing.T, region string, tags []string) string {
+func WithTags(t testing.TB, region string, tags []string) string {
 	return acceptance.ExecuteTemplate(t,
 		"reserved_ip_with_tags", TemplateData{
 			Region: region,
