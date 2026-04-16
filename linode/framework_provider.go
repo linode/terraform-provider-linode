cf18fee52f0ff3c561b8c87697e289068eff5c04
Michal Wojcik <miwojci@akamai.com>
2026-04-16 11:08:31 +0200

    TPT-4292: terraform: Implement linode_reserved_ip resource

--- linode/framework_provider.go
+++ linode/framework_provider.go
@@ -93,6 +93,7 @@ import (
 	"github.com/linode/terraform-provider-linode/v3/linode/regions"
 	"github.com/linode/terraform-provider-linode/v3/linode/regionsvpcavailability"
 	"github.com/linode/terraform-provider-linode/v3/linode/regionvpcavailability"
+	"github.com/linode/terraform-provider-linode/v3/linode/reservedip"
 	"github.com/linode/terraform-provider-linode/v3/linode/sshkey"
 	"github.com/linode/terraform-provider-linode/v3/linode/sshkeys"
 	"github.com/linode/terraform-provider-linode/v3/linode/stackscript"
@@ -259,6 +260,7 @@ func (p *FrameworkProvider) Resources(ctx context.Context) []func() resource.Res
 		placementgroup.NewResource,
 		placementgroupassignment.NewResource,
 		instancereservedipassignment.NewResource,
+		reservedip.NewResource,
 		rdns.NewResource,
 		sshkey.NewResource,
 		stackscript.NewResource,
