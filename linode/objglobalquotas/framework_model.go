c91b031ec63ea9ea164a85721dfdc2d8e417bd8b
Zhiwei Liang <zliang@akamai.com>
2026-07-09 13:26:02 -0400

    self import path updated to v4

--- linode/objglobalquotas/framework_model.go
+++ linode/objglobalquotas/framework_model.go
@@ -3,7 +3,7 @@ package objglobalquotas
 import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/linode/linodego"
-	"github.com/linode/terraform-provider-linode/v3/linode/helper/frameworkfilter"
+	"github.com/linode/terraform-provider-linode/v4/linode/helper/frameworkfilter"
 )
 
 type ObjectStorageGlobalQuotaFilterModel struct {
