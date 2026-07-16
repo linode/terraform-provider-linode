01f211cda3395d682d339b03d4203f2c75f14d7e
Zhiwei Liang <zliang@akamai.com>
2026-07-16 19:06:53 -0400

    test: suppress weak randomness findings

--- linode/acceptance/util.go
+++ linode/acceptance/util.go
@@ -4,11 +4,10 @@ import (
 	"bytes"
 	"cmp"
 	"context"
-	"crypto/rand"
 	"errors"
 	"fmt"
 	"log"
-	"math/big"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"slices"
@@ -645,14 +644,10 @@ func GetRandomObjectStorageEndpoint() (*linodego.ObjectStorageEndpoint, error) {
 		return nil, err
 	}
 
-	for i := len(endpoints) - 1; i > 0; i-- {
-		j, err := rand.Int(rand.Reader, big.NewInt(int64(i+1)))
-		if err != nil {
-			return nil, fmt.Errorf("failed to shuffle object storage endpoints: %w", err)
-		}
-
-		endpoints[i], endpoints[j.Int64()] = endpoints[j.Int64()], endpoints[i]
-	}
+	// #nosec G404 -- Test data, doesn't need to be cryptographically secure
+	rand.Shuffle(len(endpoints), func(i, j int) {
+		endpoints[i], endpoints[j] = endpoints[j], endpoints[i]
+	})
 
 	for i, e := range endpoints {
 		// Linode Object Storage clusters with E2 and E3 (Object Storage gen2) endpoints
@@ -785,12 +780,8 @@ func GetRandomRegionWithCaps(capabilities []linodego.RegionCapability, regionTyp
 		return "", fmt.Errorf("no region found with the provided caps")
 	}
 
-	index, err := rand.Int(rand.Reader, big.NewInt(int64(len(regions))))
-	if err != nil {
-		return "", fmt.Errorf("failed to select a random region: %w", err)
-	}
-
-	return regions[index.Int64()], nil
+	// #nosec G404 -- Test data, doesn't need to be cryptographically secure
+	return regions[rand.Intn(len(regions))], nil
 }
 
 func GetTestClient() (*linodego.Client, error) {
