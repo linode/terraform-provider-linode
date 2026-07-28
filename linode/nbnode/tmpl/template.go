fde81ce5005306ccc2f474dd88caf9017d4d28f6
Maciej Wilk <mawilk@akamai.com>
2026-07-28 13:34:01 +0200

    Remove unused rootPass from nbnode VPC template

--- linode/nbnode/tmpl/template.go
+++ linode/nbnode/tmpl/template.go
@@ -63,7 +63,7 @@ func Updates(t testing.TB, nodebalancer, region, rootPass, nbType string) string
 		})
 }
 
-func VPC(t testing.TB, nodebalancer, region, nbType string, _ string) string {
+func VPC(t testing.TB, nodebalancer, region, nbType string) string {
 	return acceptance.ExecuteTemplate(t,
 		"nodebalancer_node_vpc",
 		TemplateData{
